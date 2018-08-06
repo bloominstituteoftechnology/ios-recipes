5d5cab94d6b6dea44650a329c080f0d2f0ac3688
Andrew Liao <andrewoliao@gmail.com>
2018-08-06 14:18:36 -0700

    Implement all view controllers. App fully functional

--- Recipes/Recipes/View Controllers/MainViewController.swift
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -2,38 +2,72 @@
 //  MainViewController.swift
 //  Recipes
 //
-//  Created by Andrew Dhan on 8/6/18.
+//  Created by Andrew Liao on 8/6/18.
 //  Copyright © 2018 Lambda Inc. All rights reserved.
 //
 
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        networkClient.fetchRecipes { (recipe, error) in
+            if let error = error {
+                NSLog("Error: \(error)")
+            }
+            guard let recipe = recipe else {return}
+            self.allRecipes = recipe
+        }
     }
     
+    func filterRecipes(){
+        DispatchQueue.main.async {
+            
+            if let searchTerm = self.searchLabel.text,
+                searchTerm != "" {
+                self.filteredRecipes = self.allRecipes.filter{$0.name.contains(searchTerm) || $0.instructions.contains(searchTerm)}
+            } else{
+                self.filteredRecipes = self.allRecipes
+            }
+        }
+    }
     @IBAction func editingDidEnd(_ sender: Any) {
+        searchLabel.resignFirstResponder()
+        filterRecipes()
+        
     }
     
     
-    /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "EmbedTableVC"{
+            recipesTableViewController = segue.destination as! RecipesTableViewController
+            
+        }
     }
-    */
+    
+    
+    // MARK: - Properties
     @IBOutlet weak var searchLabel: UITextField!
+    let networkClient = RecipesNetworkClient()
+    var allRecipes = [Recipe]() {
+        didSet{
+            filterRecipes()
+        }
+    }
+    var recipesTableViewController: RecipesTableViewController?{
+        didSet{
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    var filteredRecipes = [Recipe](){
+        didSet{
+            recipesTableViewController?.recipes = filteredRecipes
+            recipesTableViewController?.tableView.reloadData()
+        }
+    }
     
 }
--- Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -2,7 +2,7 @@
 //  RecipeDetailViewController.swift
 //  Recipes
 //
-//  Created by Andrew Dhan on 8/6/18.
+//  Created by Andrew Liao on 8/6/18.
 //  Copyright © 2018 Lambda Inc. All rights reserved.
 //
 
