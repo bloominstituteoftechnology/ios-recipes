f3f3913b72ed40ac3d4c8e2e6fd40558283f5702
Conner Alegre <cga0@pm.me>
2018-08-06 13:18:10 -0600

    Setup storyboards with view controllers

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Conner on 8/6/18.
+//  Copyright © 2018 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet var textField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func searchRecipes(_ sender: Any) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
