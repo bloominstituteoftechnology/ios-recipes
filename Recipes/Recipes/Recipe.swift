9e1d46e8069f12d485f6c98d15834f50d12536fa
William Bundy <williambundy@icloud.com>
2018-08-06 12:06:25 -0700

    Some initial tweaks...

--- Recipes/Recipes/Recipe.swift
+++ Recipes/Recipes/Recipe.swift
@@ -7,9 +7,13 @@
 
 import Foundation
 
-struct Recipe: Codable {
-    
+struct Recipe: Codable, Comparable
+{
     var name: String
     var instructions: String
-    
+
+	static func <(l:Recipe, r:Recipe) -> Bool
+	{
+		return l.name < r.name
+	}
 }
