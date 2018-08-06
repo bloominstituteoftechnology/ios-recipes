acf00daa8a9b2b60af7ca9bb1c9b8502598a631b
Andrew Liao <andrewoliao@gmail.com>
2018-08-06 13:28:53 -0700

    implement RecipeDetailVC

--- Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -9,8 +9,26 @@
 import UIKit
 
 class RecipeDetailViewController: UIViewController {
-
-
+    
+    
+    func updateViews(){
+        guard let recipe = recipe, isViewLoaded else {return}
+        titleLabel.text = recipe.name
+        textView.text = recipe.instructions
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViews()
+    }
+    
+    //MARK: Properties
+    var recipe:Recipe?{
+        didSet{
+            updateViews()
+        }
+    }
+    
     @IBOutlet weak var titleLabel: UILabel!
     
     @IBOutlet weak var textView: UITextView!
