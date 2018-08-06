95eb79d044617d9c9ed4ee47bc99d88f331d3555
Conner Alegre <cga0@pm.me>
2018-08-06 14:38:04 -0600

    Complete project with search functionality

--- Recipes/Recipes/View Controllers/MainViewController.swift
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -9,23 +9,60 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    var recipesTableViewController: RecipesTableViewController?
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    var networkClient = RecipesNetworkClient()
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
+    }
+    
     @IBOutlet var textField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        networkClient.fetchRecipes { (recipes, error) in
+            if let error = error {
+                NSLog("\(error)")
+                return
+            } else {
+                if let recipes = recipes {
+                    self.allRecipes = recipes
+                }
+            }
+        }
     }
     
     @IBAction func searchRecipes(_ sender: Any) {
-        
+        filterRecipes()
     }
-    /*
+    
+    func filterRecipes() {
+        DispatchQueue.main.async {
+            if let searchTerm = self.textField.text {
+                if searchTerm.count == 0 {
+                    self.filteredRecipes = self.allRecipes
+                } else {
+                    self.filteredRecipes = self.allRecipes.filter({ $0.name.contains(searchTerm) || $0.instructions.contains(searchTerm) })
+                }
+            }
+        }
+
+    }
+    
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if (segue.identifier == "RecipesEmbedSegue") {
+            recipesTableViewController = segue.destination as? RecipesTableViewController
+        }
     }
-    */
 
 }
--- Recipes/Recipes/View Controllers/RecipesTableViewController.swift
+++ Recipes/Recipes/View Controllers/RecipesTableViewController.swift
@@ -12,13 +12,14 @@ class RecipesTableViewController: UITableViewController {
     
     var recipes: [Recipe] = [] {
         didSet {
-            tableView.reloadData()
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -28,7 +29,6 @@ class RecipesTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath)
-        
         cell.textLabel?.text = recipes[indexPath.row].name
         
         return cell
@@ -37,14 +37,9 @@ class RecipesTableViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowRecipeDetailSegue" {
             let recipesDetailVC = segue.destination as! RecipeDetailViewController
-            if let tappedOnRecipe = sender as? Recipe {
-                recipesDetailVC.recipe = tappedOnRecipe
+            if let index = self.tableView.indexPathForSelectedRow {
+                recipesDetailVC.recipe = recipes[index.row]
             }
         }
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let recipe = recipes[indexPath.row]
-        performSegue(withIdentifier: "ShowRecipeDetailSegue", sender: recipe)
     }
 }
