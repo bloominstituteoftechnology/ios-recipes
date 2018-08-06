838bda06adf1f8e6625fbbd9c8286d44d789a706
Andrew Liao <andrewoliao@gmail.com>
2018-08-06 13:05:24 -0700

    Implement View Controllers

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Andrew Dhan on 8/6/18.
+//  Copyright © 2018 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func editingDidEnd(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBOutlet weak var searchLabel: UITextField!
+    
+}
---
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Andrew Dhan on 8/6/18.
+//  Copyright © 2018 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var textView: UITextView!
+}
