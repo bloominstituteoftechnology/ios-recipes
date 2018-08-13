a34dafe1b5b50f426149ec634d37727261dde8eb
Lisa Sampson <lisasampson0918@gmail.com>
2018-08-13 17:01:50 -0400

    Set up table view

--- Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -16,22 +16,6 @@ class RecipeDetailViewController: UIViewController {
         updateViews()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     func updateViews() {
         if isViewLoaded {
             guard let recipe = recipe else { return }
