85a586055bab9933e1805f3ce565972e1ff2f31d
Farhan Farooqui <farhan@farooqui.net>
2018-09-10 17:45:28 -0400

    storyboards ready

---
+++ Recipes/Recipes/VCs/RecipeDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Farhan on 9/10/18.
+//  Copyright © 2018 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var recipeTextView: UITextView!
+}
