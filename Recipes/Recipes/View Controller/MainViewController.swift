636da87955a1e3b28e9046e805a4f92d05492043
iyinraphael <iyinraphael@gmail.com>
2018-09-17 14:33:06 -0700

    finished developing app

--- Recipes/Recipes/View Controller/MainViewController.swift
+++ Recipes/Recipes/View Controller/MainViewController.swift
@@ -12,12 +12,55 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        networkClient.fetchRecipes { (allRecipes, error) in
+            if let error = error{
+                NSLog("Error ocured while fetching data: \(error)")
+                return
+            }else{
+                self.allRecipes = (allRecipes ?? nil)!
+            }
+        }
     }
     @IBOutlet weak var textField: UITextField!
     
+    @IBAction func textFieldAction(_ sender: Any) {
+        resignFirstResponder()
+        filterRecipe()
+    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            guard let _ = segue.destination as? RecipesTableViewController else {return}
+        }
+    }
+    func filterRecipe(){
+        DispatchQueue.main.async {
+            guard let text = self.textField.text, text != "" else {
+                self.filteredRecipes = self.allRecipes
+                return }
+            
+            let lookUpRecipies = self.allRecipes.filter({$0.name.contains(text) || $0.instructions.contains(text)})
+            self.filteredRecipes = lookUpRecipies
+            
+        }
+    }
     
+    
+    var filteredRecipes = [Recipe](){
+        didSet{
+            recipesTableViewController.recipe = self.filteredRecipes
+        }
+    }
+    var allRecipes = [Recipe](){
+        didSet{
+            filterRecipe()
+        }
+    }
+    let networkClient = RecipesNetworkClient()
+    var recipesTableViewController = RecipesTableViewController(){
+        didSet{
+           recipesTableViewController.recipe = self.filteredRecipes
+        }
     }
 
 }
