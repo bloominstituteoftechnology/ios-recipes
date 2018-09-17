70101833fbe6b412dcac1b766ca9baa5c418c4e9
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-17 18:03:23 -0500

    Storyboard set up completed. Related Cocoa subclass files created. Outlets and actions connected

---
+++ Recipes/Recipes/View Controller/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Ilgar Ilyasov on 9/17/18.
+//  Copyright © 2018 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    
+    @IBOutlet weak var recipeTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recipeTextFieldAction(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
