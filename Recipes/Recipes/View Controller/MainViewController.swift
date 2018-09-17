7a5e9f96dc2eeed926f6adf7a916c0d7906b410c
iyinraphael <iyinraphael@gmail.com>
2018-09-17 13:05:32 -0700

    views create din storyboard and connected to respective view controller

---
+++ Recipes/Recipes/View Controller/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Iyin Raphael on 9/17/18.
+//  Copyright © 2018 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBOutlet weak var textField: UITextField!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+    }
+
+}
