6c0620fc6433ae17efd465b4868650f64db8c13f
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-17 20:42:21 -0700

    RecipesDetailVC completed

--- Recipes/View Controllers/RecipeDetailViewController.swift
+++ Recipes/View Controllers/RecipeDetailViewController.swift
@@ -35,17 +35,5 @@ class RecipeDetailViewController: UIViewController {
     @IBOutlet weak var recipeLabel: UILabel!
     
     @IBOutlet weak var instructionsTextView: UITextView!
-    
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
