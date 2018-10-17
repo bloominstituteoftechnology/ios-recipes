04c50729d78d7adffaef9f49bd5e57ca40e99e03
OmegaWulf <nikcxz@live.com>
2018-10-17 16:28:24 -0400

    Finished Main ViewController

--- Recipes/Recipes/ViewController/MainViewController.swift
+++ Recipes/Recipes/ViewController/MainViewController.swift
@@ -9,19 +9,71 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     @IBOutlet weak var textField: UITextField!
     @IBAction func finishedEditingText(_ sender: Any) {
+        resignFirstResponder()
+        filterRecipes()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    let networkClient = RecipesNetworkClient()
+    var allRecipes = [Recipe]() {
+        didSet {
+            filterRecipes()
+        }
     }
-    */
+    
+    
+    var recipeTableViewController: RecipeTableViewController? {
+        didSet {
+            recipeTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
+    var filteredRecipes = [Recipe]() {
+        didSet {
+            recipeTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        networkClient.fetchRecipes { recipes, error in
+            if error != nil {
+                NSLog("error: \(error!)")
+                return
+            } else {
+                if let recipes = recipes {
+                    self.allRecipes = recipes
+                }
+            }
+        }
+    }
+    
+    func filterRecipes() {
+        guard let text = textField.text else {return}
+        if text.isEmpty {
+            filteredRecipes = allRecipes
+        } else {
+            filteredRecipes = allRecipes.filter({ $0.name.contains(text) || $0.instructions.contains(text) })
+        }
+    }
+    
+    
+     // MARK: - Navigation
+     
 
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+        
+        guard let destination = segue.destination as? RecipeTableViewController else {return}
+        if segue.identifier == "toTableView" {
+            destination.recipes = allRecipes
+        }
+        
+     }
+
+    
 }
