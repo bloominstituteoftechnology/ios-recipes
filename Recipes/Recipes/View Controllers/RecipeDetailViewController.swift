6c5a8fd0892f7362cb29f00d0dd15b6eea8ae588
Paul Yi <yipaulc@gmail.com>
2018-10-17 12:58:10 -0700

    Create storyboard layout

---
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Paul Yi on 10/17/18.
+//  Copyright © 2018 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var recipeTitle: UILabel!
+    
+    @IBOutlet weak var recipeTextView: UITextView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
