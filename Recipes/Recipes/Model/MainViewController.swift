eb3b6155486c7462f92ed2478487d2f75e879261
Frulwinn <Frulwinn@gmail.com>
2018-10-17 12:59:32 -0700

    Setup main story board and swift files to identify classes

---
+++ Recipes/Recipes/Model/MainViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func textField(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
---
+++ Recipes/Recipes/Model/RecipeDetailViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
