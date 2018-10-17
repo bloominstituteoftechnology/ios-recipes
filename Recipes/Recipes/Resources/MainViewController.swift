cee92b9c6547b659e345996f2fbd099f252404a4
yzhukovs <yzhukovs@bumnetworks.com>
2018-10-17 17:32:38 -0400

    more updates

--- Recipes/Recipes/Resources/MainViewController.swift
+++ Recipes/Recipes/Resources/MainViewController.swift
@@ -9,13 +9,45 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    let networkClient = RecipesNetworkClient()
+    
+    var filteredRecipes: [Recipe] = []
+    
+    
+    
+    
+    var allRecipes: [Recipe] = []
+    
+    
+    
+    var recipesTableViewController: RecipesTableViewController?
+    
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "recipes") {
+            recipesTableViewController = segue.destination as? RecipesTableViewController
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        networkClient.fetchRecipes { (recipes, error) in
+            if let error = error {
+                NSLog("some error\(error)")
+                return
+            } else {
+                if let recs = recipes {
+                    self.allRecipes = recs
+                }
+            }
+            
+        }
+        
         // Do any additional setup after loading the view.
     }
-   
+    
     
     
     @IBOutlet weak var textField: UITextField!
@@ -24,15 +56,15 @@ class MainViewController: UIViewController {
     @IBAction func editingDidEnd(_ sender: Any) {
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- Recipes/Recipes/Resources/RecipeDetailViewController.swift
+++ Recipes/Recipes/Resources/RecipeDetailViewController.swift
@@ -9,27 +9,55 @@
 import UIKit
 
 class RecipeDetailViewController: UIViewController {
-
+    
+    var recipe: Recipe? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    
+    
+    
+    func updateViews(){
+        
+        if let rec = recipe {
+            if self.isViewLoaded {
+                label.text = rec.name
+                textView.text = rec.instructions
+            }
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateViews()
         // Do any additional setup after loading the view.
     }
     
-
+    
     @IBOutlet weak var label: UILabel!
     
     
     @IBOutlet weak var textView: UITextView!
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
