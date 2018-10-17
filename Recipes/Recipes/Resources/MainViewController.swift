3c33e41b8b2870d754d2cf7453960110d9aca02b
Sean Doyle <sdoylelambda@gmail.com>
2018-10-17 15:50:16 -0400

    initial commit

---
+++ Recipes/Recipes/Resources/MainViewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var outletTextField: UITextField!
+    
+    
+    @IBAction func actionTextField(_ sender: Any) {
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+
+}
