725055e161cf6c1abeac1cd884e10f1742919cf5
Paul Yi <yipaulc@gmail.com>
2018-10-17 14:16:05 -0700

    Write implementation of MainViewController

--- Recipes/Recipes/View Controllers/MainViewController.swift
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -2,19 +2,60 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    let networkClient = RecipesNetworkClient()
     
-    @IBOutlet weak var recipe: UITextField!
-    
-    @IBAction func recipeTapped(_ sender: Any) {
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
     }
     
-    
-    
+    @IBOutlet weak var recipe: UITextField!
+    @IBAction func recipeTapped(_ sender: Any) {
+        recipe.resignFirstResponder()
+        filterRecipes()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        networkClient.fetchRecipes { (success, error) in
+            if let error = error {
+                NSLog("Error uploading recipes: \(error)")
+                return
+            }
+            self.allRecipes = success ?? []
+        }
+    }
+    
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowRecipeSegue" {
+            guard let recipeTVC = segue.destination as? RecipesTableViewController else { return }
+            recipesTableViewController = recipeTVC
+        }
+    }
+    
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
+    func filterRecipes() {
+        DispatchQueue.main.async {
+            if let search = self.recipe.text, search.count > 0 {
+                self.filteredRecipes = self.allRecipes.filter({ (recipe) -> Bool in
+                    return recipe.name.contains(search) || recipe.instructions.contains(search)
+                })
+            } else {
+                self.filteredRecipes = self.allRecipes
+            }
+        }
     }
     
 
