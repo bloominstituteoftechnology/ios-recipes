35eb2a15420a5d549098b3b16f0614ea19b81b28
Paul Yi <yipaulc@gmail.com>
2018-10-17 13:15:31 -0700

    Write implementation of RecipeDetailViewController

--- Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -1,25 +1,33 @@
-//
-//  RecipeDetailViewController.swift
-//  Recipes
-//
-//  Created by Paul Yi on 10/17/18.
-//  Copyright © 2018 Lambda Inc. All rights reserved.
-//
-
 import UIKit
 
 class RecipeDetailViewController: UIViewController {
-
+    
+    var recipe: Recipe? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    func updateViews() {
+        if isViewLoaded {
+            guard let recipe = recipe else { return }
+            recipeTitle?.text = recipe.name
+            recipeTextView?.text = recipe.instructions
+        }
+    }
     
     @IBOutlet weak var recipeTitle: UILabel!
-    
     @IBOutlet weak var recipeTextView: UITextView!
+    
+    
     
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateViews()
 
         // Do any additional setup after loading the view.
     }
