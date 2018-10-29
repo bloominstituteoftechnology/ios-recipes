ecf60d6c0cb81b57fa8aa6eba6474c37ec87ac12
Moses Robinson <mosesjrobinson@icloud.com>
2018-10-29 16:38:27 -0700

    implementing main view controller again

--- Recipes/Recipes/ViewController/RecipesTableViewController.swift
+++ Recipes/Recipes/ViewController/RecipesTableViewController.swift
@@ -2,5 +2,34 @@ import UIKit
 
 class RecipesTableViewController: UITableViewController {
     
+    var recipes: [Recipe] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return recipes.count
+    }
+    
+    let reuseIdentifier = "recipeCell"
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        
+        let recipe = recipes[indexPath.row]
+        cell.textLabel?.text = recipe.name
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            
+            guard let destinationView = segue.destination as? RecipeDetailViewController,
+                let indexPath = tableView.indexPathForSelectedRow else { return }
+            
+            destinationView.recipe = recipes[indexPath.row]
+    }
 }
+
+
--- Recipes/Recipes/ViewControllers/RecipeDetailViewController.swift
+++ Recipes/Recipes/ViewControllers/RecipeDetailViewController.swift
@@ -2,7 +2,23 @@ import UIKit
 
 class RecipeDetailViewController: UIViewController {
     
+    var recipe: Recipe?
+    
     @IBOutlet weak var recipeNameLabel: UILabel!
     @IBOutlet weak var instructionsTextView: UITextView!
+    
+    func updateViews() {
+        if isViewLoaded {
+            guard let recipe = recipe else { return }
+            
+            recipeNameLabel.text = recipe.name
+            instructionsTextView.text = recipe.instructions
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViews()
+    }
     
 }
