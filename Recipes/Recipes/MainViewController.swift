c818acc591402c367df7b48ff20dbc3a3628e4a9
Frulwinn <frulwinn@gmail.com>
2018-10-29 15:27:43 -0700

    Added elements to Main storyboard, identified the class for each controller, and made outlets

--- Recipes/Recipes/MainViewController.swift
+++ Recipes/Recipes/MainViewController.swift
@@ -1,5 +1,27 @@
 import UIKit
 
 class MainViewController: UIViewController {
+
+    @IBOutlet weak var recipeTextField: UITextField!
     
+    @IBAction func recipeTextField(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
--- Recipes/Recipes/RecipeDetailViewController.swift
+++ Recipes/Recipes/RecipeDetailViewController.swift
@@ -2,5 +2,29 @@ import UIKit
 
 class RecipeDetailViewController: UIViewController {
     
+    var recipe: Recipe?
+
+    @IBOutlet weak var recipeName: UILabel!
     
+    @IBOutlet weak var recipeDetails: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    func updateViews(){
+        guard let recipe = Recipe(name:String, instructions: String )
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
