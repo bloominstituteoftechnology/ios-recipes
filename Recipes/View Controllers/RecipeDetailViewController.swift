943ee4616b63d007bddebbe3b903a2c46b471c8e
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-12-03 13:32:28 -0700

    Added updateViews function.

--- Recipes/View Controllers/RecipeDetailViewController.swift
+++ Recipes/View Controllers/RecipeDetailViewController.swift
@@ -2,8 +2,23 @@ import UIKit
 import Foundation
 
 class RecipeDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    var recipe: Recipe?
+    
     @IBOutlet weak var label: UILabel!
     
     @IBOutlet weak var recipeText: UITextView!
+    
+    func updateViews() {
+        if isViewLoaded {
+            guard let recipe = recipe else { return }
+            label.text = recipe.name
+            recipeText.text = recipe.instructions
+        }        
+    }
     
 }
