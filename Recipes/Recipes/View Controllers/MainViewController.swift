035c4ff88f6c80cbc9dd11642fe1a0b1fab0964f
Ben Hakes <bhhakes@gmail.com>
2018-12-03 15:00:37 -0600

    set up main vc

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Benjamin Hakes on 12/3/18.
+//  Copyright © 2018 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func editingDidEnd(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
