8b7bedd80f65c927fdb2c190d7b894d88e6ce2a0
Meera Andersen <meera.andersen@gmail.com>
2018-12-03 13:19:52 -0700

    Set Built out UI, and add outlet and action for text field

---
+++ Recipe Project/Recipe Project/MainViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func textFieldSearch(_ sender: Any) {
+        
+    }
+}
---
+++ Recipe Project/Recipe Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recipe Project
+//
+//  Created by Meera Andersen on 12/3/18.
+//  Copyright © 2018 Meera Andersen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
