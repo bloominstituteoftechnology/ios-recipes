f1d8533ab8cf6fb48fc30aec7b577605df55814d
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-12-03 14:10:23 -0700

    Fetched recipes from the network client and logged it incase there was an error.

--- Recipes/View Controllers/MainViewController.swift
+++ Recipes/View Controllers/MainViewController.swift
@@ -3,6 +3,22 @@ import Foundation
 
 class MainViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        networkClient.fetchRecipes { (recipes, error) in
+            if let error = error {
+                NSLog("Error fetching recipes \(error)")
+                return
+            } else {
+                self.allRecipes = recipes ?? []
+            }
+        }
+    }
+    
+    private let networkClient = RecipesNetworkClient()
+    
+    var allRecipes: [Recipe] = []
+    
     @IBOutlet weak var textField: UITextField!
     
     @IBAction func didTapText(_ sender: Any) {
