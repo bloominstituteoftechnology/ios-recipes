a56a6d2fa8dea47693b57c43f4f0d2428b578915
Frulwinn <frulwinn@gmail.com>
2018-12-03 13:18:47 -0800

    hooked up outlets for label and view to DetailViewController

---
+++ Recipes/Recipes/RecipeDetailViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    @IBOutlet weak var recipeLabel: UILabel!
+    
+    @IBOutlet weak var recipeView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
