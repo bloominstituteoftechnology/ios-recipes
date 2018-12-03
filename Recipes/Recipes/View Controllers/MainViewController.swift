9776be7c499ada646c45aba1315ecc257163f0e4
AustinBCole <austin.cole.chileno@gmail.com>
2018-12-03 16:47:43 -0700

    Implement table view and main view ui.

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Austin Cole on 12/3/18.
+//  Copyright © 2018 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func sortByUserString(_ sender: Any) {
+        
+    }
+}
