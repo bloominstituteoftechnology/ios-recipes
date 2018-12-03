93ee928482aebd73de01fe2a02c24a455453b167
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2018-12-03 17:02:08 -0500

    update storyboard with connections and add new files

---
+++ Recipes/Recipes/RecipeDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Julian A. Fordyce on 12/3/18.
+//  Copyright © 2018 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextView!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
