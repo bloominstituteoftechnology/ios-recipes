358785167f881bc41079d7f8c5d1c51d99d65086
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-12-03 14:05:20 -0700

    Segue function completed.

--- Recipes/View Controllers/RecipesTableViewController.swift
+++ Recipes/View Controllers/RecipesTableViewController.swift
@@ -21,4 +21,15 @@ class RecipesTableViewController: UITableViewController {
         return cell
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Destination for the segue is the RecipeDetailViewController
+        guard let destination = segue.destination as? RecipeDetailViewController else { return }
+        // If the cells sender is from the TableViewCell, it'll be on the indexPath for the selected cell.
+        if let cell = sender as? UITableViewCell {
+            guard let indexPath = tableView.indexPath(for: cell) else { return }
+            
+            destination.recipe = recipes[indexPath.row]
+        }
+    
+}
 }
