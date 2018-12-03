de82bcefe587b344086eb7b102f774e178aded34
Meera Andersen <meera.andersen@gmail.com>
2018-12-03 16:32:36 -0700

    Add segue and viewDidLoad functionality

--- Recipe Project/Recipe Project/ViewControllers/MainViewController.swift
+++ Recipe Project/Recipe Project/ViewControllers/MainViewController.swift
@@ -9,7 +9,6 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-  
 // in closures you always have to declare self
         networkClient.fetchRecipes { (recipes, error) in
             if let error = error {
@@ -19,7 +18,29 @@ class MainViewController: UIViewController {
             self.allRecipes = recipes ?? []
         }
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EmbededRecipeTableView" {
+            let recipesTableVC = segue.destination as! RecipesTableViewController
+            recipesTableViewController = recipesTableVC
+        }
+    }
+    
     private var allRecipes: [Recipe] = [] 
     private let networkClient = RecipesNetworkClient()
+    private var recipesTableViewController: RecipesTableViewController
+    private var filteredREcipes: [Recipe] = []
+    
+    func filterRecipes() {
+        guard let text = textField.text, !text.isEmpty else { return }
+        
+        
+    }
     
 }
+//override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//    if segue.identifier == "EmbedStudentsTableView" {
+//        let studentsTableVC = segue.destination as! StudentsTableViewController
+//        studentsTableViewController = studentsTableVC
+//    }
+//}
