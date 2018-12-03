91a62bde604f5b09c6894a2f6c0b681f0bdd5bae
Sergey Osipyan <osipyansergey@gmail.com>
2018-12-03 14:27:44 -0800

    Finish Part 1 (added all views and objects)

---
+++ Recipes/Recipes/View Controllers/RecipeDatailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RecipeDatailViewController.swift
+//  Recipes
+//
+//  Created by Sergey Osipyan on 12/3/18.
+//  Copyright © 2018 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDatailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textView: UITextView!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
