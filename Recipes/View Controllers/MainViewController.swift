f28f410458681956ded3f6982920dc40d7cede28
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-12-04 15:11:57 -0800

    add storyboard layout
    and fix a small typo in the README

---
+++ Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Jaspal on 12/4/18.
+//  Copyright © 2018 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var seachTextField: UITextField!
+    @IBAction func searchTextField(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
