b2615b1eaf1ed3b8819aad21868f16d376a41c60
jkaunert <jkaunert@clusterfly.net>
2018-12-03 18:29:52 -0600

    adds storyboard layout  and implements app skeleton (part 1)

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var recipeSearchField: UITextField!
+    
+    
+    @IBAction func search(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
