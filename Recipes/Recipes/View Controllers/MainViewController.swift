895a9c8ff29bf312559c5993e188e58d8d702827
AustinBCole <austin.cole.chileno@gmail.com>
2018-12-04 08:27:18 -0700

    almost implement initial functionality.

--- Recipes/Recipes/View Controllers/MainViewController.swift
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -12,23 +12,73 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        networkClient.fetchRecipes{ (allRecipes, error) in
+            if let error = error {
+                NSLog("Error getting students: \(error)")
+                return
+            }
+            self.allRecipes = allRecipes ?? []
+            
+        }
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "showDetailViewControllerSegue" {
+            let recipesTableVC = segue.destination as! RecipesTableViewController
+            recipesTableViewController = recipesTableVC
+        }
     }
-    */
+    
+    let networkClient = RecipesNetworkClient()
+    
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
+    }
+    
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
     @IBOutlet weak var textField: UITextField!
     
     @IBAction func sortByUserString(_ sender: Any) {
         
+    resignFirstResponder()
+        filterRecipes()
+        
     }
+    
+    func filterRecipes() {
+        
+        if textField.text == nil {filteredRecipes = allRecipes}
+        else {
+            let text = (self.textField?.text)
+            guard let greatText = text else {fatalError("Could not get text.")}
+            for recipe in self.allRecipes {
+                
+                self.filteredRecipes += self.allRecipes.filter{ (recipe) -> Bool in recipe.name.contains(Character(greatText) )}
+                
+                self.filteredRecipes += self.allRecipes.filter{ (recipe) -> Bool in recipe.instructions.contains(Character(greatText))}
+                }
+        
+        
+            }
+        }
 }
---
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,47 @@
+//
+//  DetailViewController.swift
+//  Recipes
+//
+//  Created by Austin Cole on 12/3/18.
+//  Copyright © 2018 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViews()
+
+        // Do any additional setup after loading the view.
+    }
+
+    func updateViews() {
+        if isViewLoaded {
+        textLabel.text = recipe?.name
+        textView.text = recipe?.instructions
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    var recipe: Recipe? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    
+}
