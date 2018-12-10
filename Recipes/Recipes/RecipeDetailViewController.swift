4798aee659868d0504b654fa6268e50436da14d9
Frulwinn <frulwinn@gmail.com>
2018-12-10 13:35:41 -0800

    added func updateViews to RecipeDetailViewController

--- Recipes/Recipes/RecipeDetailViewController.swift
+++ Recipes/Recipes/RecipeDetailViewController.swift
@@ -1,6 +1,13 @@
 import UIKit
 
 class RecipeDetailViewController: UIViewController {
+    
+    var recipe = Recipe? {
+        didSet {
+            updateViews()
+        }
+    }
+    
     @IBOutlet weak var recipeLabel: UILabel!
     
     @IBOutlet weak var instructionView: UITextView!
@@ -8,19 +15,14 @@ class RecipeDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        if isViewLoaded {
+            guard let recipe = recipe else {return}
+            recipeLabel.text = recipe.name
+            instructionView.text = recipe.instructions
+        }
     }
-    */
-
 }
