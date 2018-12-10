605751fdb04685a1880a9114e0bd8a1fbdf7c234
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-12-10 13:56:09 -0800

    complete main view controller UI

---
+++ ios-recipes-master/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Jaspal on 12/10/18.
+//  Copyright © 2018 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func searchRecipe(_ sender: Any) {
+        
+    }
+    
+}
