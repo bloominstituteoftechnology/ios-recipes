5b2702e9ad63987631f16f5788b6cffcfc873b9b
Frulwinn <frulwinn@gmail.com>
2018-12-10 13:04:36 -0800

    initial commit

--- Recipes/Recipes/RecipeDetailViewController.swift
+++ Recipes/Recipes/RecipeDetailViewController.swift
@@ -2,27 +2,5 @@ import UIKit
 
 class RecipeDetailViewController: UIViewController {
     
-    var recipe: Recipe? {
-        //didSet gets called once recipe property is set
-        didSet {
-            updateViews()
-        }
-    }
-    
-    @IBOutlet weak var recipeLabel: UILabel!
-    
-    @IBOutlet weak var recipeView: UITextView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        updateViews()
-    }
-    
-    func updateViews() {
-        if isViewLoaded {
-        guard let recipe = recipe else {return}
-        recipeLabel.text = recipe.name
-        recipeView.text = recipe.instructions
-        }
-    }
+   
 }
--- Recipes/Recipes/RecipesTableViewController.swift
+++ Recipes/Recipes/RecipesTableViewController.swift
@@ -2,40 +2,5 @@ import UIKit
 
 class RecipesTableViewController: UITableViewController {
     
-    let reuseIdentifier = "recipeCell"
     
-    var recipes: [Recipe] = [] {
-        //didSet is called when the recipes property is set
-        didSet {
-            //reloading data needs to happen on the Main thread
-            DispatchQueue.main.async {
-            self.tableView.reloadData()
-            }
-        }
-    }
-
-    // MARK: - Table view data source
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return recipes.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
-        //let recipe = recipes[indexPath.row]
-        //cell.textLabel?.text = recipe.name
-        cell.textLabel?.text = recipes[indexPath.row].name
-        
-        return cell
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "recipeCellSegue" {
-        guard let indexPath = tableView.indexPathForSelectedRow else {return}
-        
-        guard let recipeDestination = segue.destination as?
-                RecipeDetailViewController else { return }
-            
-        recipeDestination.recipe = recipes[indexPath.row]
-        }
-    }
 }
