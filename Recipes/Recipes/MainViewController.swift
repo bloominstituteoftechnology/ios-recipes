174bdbb8ff16ffa8ac84ae89f0cf2e8eb8658a99
Frulwinn <frulwinn@gmail.com>
2018-12-10 14:44:00 -0800

    added filterRecipes func to MainViewController

--- Recipes/Recipes/MainViewController.swift
+++ Recipes/Recipes/MainViewController.swift
@@ -3,8 +3,14 @@ import UIKit
 class MainViewController: UIViewController {
     
     let networkClient = RecipesNetworkClient()
+    
     var allRecipes: [Recipe] = []
+    
     var recipesTableViewController: RecipesTableViewController?
+    
+    var filteredRecipes: [Recipe] = []
+    
+    @IBOutlet weak var searchField: UITextField!
     
     @IBAction func search(_ sender: Any) {
     }
@@ -20,12 +26,21 @@ class MainViewController: UIViewController {
             self.allRecipes = recipes ?? []
         }
     }
+    
+    func filterRecipes() {
+        if let search = searchField.text, search.count > 0 {
+            filteredRecipes = allRecipes.filter {
+                $0.name.contains(search) || $0.instructions.contains(search) }
+        } else {
+            filteredRecipes = allRecipes
+            }
+        }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "detailSegue" {
             guard let mainDestination = segue.destination as? RecipesTableViewController else {return}
             
-            RecipesTableViewController = mainDestination
+            recipesTableViewController = mainDestination
         }
     }
 }
