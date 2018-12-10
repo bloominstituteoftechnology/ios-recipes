fe54bf15df9fac4705a9ec64a57bf20b9e2f5f50
Sean Doyle <sdoylelambda@gmail.com>
2018-12-10 16:08:03 -0500

    Part 1 completed: storyboard set up and files needed added skeletons

--- Recipes/Recipes/AppDelegate.swift
+++ Recipes/Recipes/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  Recipes
-//
-//  Created by Andrew R Madsen on 8/5/18.
-//  Copyright © 2018 Lambda Inc. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
---
+++ Recipes/Recipes/Resources/MainViewController.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var recipeText: UITextField!
+    
+    @IBAction func recipeTextAction(_ sender: Any) {
+        
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
