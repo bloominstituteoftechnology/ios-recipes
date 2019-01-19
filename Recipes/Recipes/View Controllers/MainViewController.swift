73fc1033f87cf9bcba35c43d5549e3e39827bc9a
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-19 11:52:31 -0500

    Add scenes with respective classes and outlets/actions

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Nelson Gonzalez on 1/19/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    @IBAction func searchTextExit(_ sender: UITextField) {
+    }
+    
+}
