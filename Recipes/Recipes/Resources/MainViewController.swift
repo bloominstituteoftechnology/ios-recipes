d54a0e9f2e9c90ba90bd28e2ddc32774d9be6ee9
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-21 09:30:56 -0800

    Set up mainVC.

--- Recipes/Recipes/Resources/MainViewController.swift
+++ Recipes/Recipes/Resources/MainViewController.swift
@@ -12,14 +12,53 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        networkClient.fetchRecipes { (recipes, error) in
+            if let error = error {
+                NSLog("Error getting recipes: \(error)")
+                return
+            }
+            self.allRecipes = recipes ?? []
+        }
     }
     
     @IBAction func beginSearch(_ sender: UITextField) {
+        resignFirstResponder()
+        filterRecipes()
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TableEmbed" {
+            let recipesTableVC = segue.destination as! RecipesTableViewController
+            recipesTableVC.recipes = filteredRecipes
+        }
+    }
+    
+    func filterRecipes() {
+        if let searchTerm = searchField.text {
+            if searchTerm.isEmpty {
+                filteredRecipes = allRecipes
+            } else {
+                filteredRecipes = allRecipes.filter { $0.name.contains(searchTerm) || $0.instructions.contains(searchTerm) }
+            }
+        }
+    }
+    
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
     @IBOutlet weak var searchField: UITextField!
     
-
+    let networkClient = RecipesNetworkClient()
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.filterRecipes()
+            }
+        }
+    }
+    var recipesTableViewController: RecipesTableViewController?
 }
