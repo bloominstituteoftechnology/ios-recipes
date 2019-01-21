baef7fd2b80aff1c1f3da6485245c495fce97e60
Jocelyn Stuart <luluscomputer@jocelyns-macbook-air.local>
2019-01-21 17:34:36 -0500

    adds implementation of the view controller swift files

--- Recipes/Recipes/Resources/MainViewController.swift
+++ Recipes/Recipes/Resources/MainViewController.swift
@@ -12,8 +12,17 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        networkClient.fetchRecipes { (recipes, error) in
+            if let error = error {
+                NSLog("Error getting recipes: \(error)")
+                return
+            }
+            
+            self.allRecipes = recipes ?? []
+            
+        }
+        
     }
     
     
@@ -21,17 +30,55 @@ class MainViewController: UIViewController {
     
     
     @IBAction func searchEdited(_ sender: UITextField) {
+        resignFirstResponder()
+        filterRecipes()
     }
     
+    let networkClient = RecipesNetworkClient()
+    
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
+    }
+    
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet {
+            recipesTableViewController!.recipes = filteredRecipes
+        }
+    }
+    
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController!.recipes = filteredRecipes
+        }
+    }
+    
+    
+  
+    
+    func filterRecipes() {
+        guard let searchText = searchTextField.text else {return}
+        
+        if !searchText.isEmpty {
+            let filteredRecipes = allRecipes.filter { (recipe) -> Bool in contains(searchText as! UIFocusEnvironment) }
+        } else {
+            filteredRecipes = allRecipes
+        }
+        recipesTableViewController!.recipes = filteredRecipes
+        
+    }
 
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "embedRecipes" {
+            let recipesTableVC = segue.destination as! RecipesTableViewController
+            recipesTableViewController = recipesTableVC
+        }
     }
-    */
+    
 
 }
