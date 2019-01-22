d5a918d4ea6a8708f560e598ed5bad4570a53209
Moses Robinson <mosesjrobinson@icloud.com>
2019-01-21 20:13:01 -0800

    setting up filter funtion

--- Recipes/Recipes/View Controllers/MainViewController.swift
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -12,21 +12,51 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        networkClient.fetchRecipes { (recipes, error) in
+            if let error = error {
+                NSLog("error getting recipes: \(error)")
+                return
+            } else {
+                self.allRecipes = recipes ?? []
+            }
+        }
     }
     
     @IBAction func searchRecipe(_ sender: Any) {
         
     }
     
+    private func filterRecipes() {
+        guard let searchTerm = searchRecipesField.text?.lowercased(),
+            !searchTerm.isEmpty else { return }
+        
+        if searchTerm.isEmpty {
+            filteredRecipes = allRecipes
+        } else {
+            
+        }
+    }
     
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "EmbedRecipesTableView" {
+            guard let recipesTableVC = segue.destination as? RecipesTableViewController else { return }
+            
+            recipesTableViewController = recipesTableVC
+        }
     }
     
     //MARK: - Properties
+    
+    let networkClient = RecipesNetworkClient()
+    
+    var allRecipes: [Recipe] = []
+    
+    var recipesTableViewController: RecipesTableViewController?
+    
+    var filteredRecipes: [Recipe] = []
     
     @IBOutlet weak var searchRecipesField: UITextField!
     
