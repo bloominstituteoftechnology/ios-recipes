5162c58941d762bb9de58bb54dd585d8ddd34f51
Angel <angelbuenrostro@outlook.com>
2019-01-22 01:15:01 -0500

    Finish networking and make sure UI updating is handled on the main thread. Completed Project.

--- Recipes/Recipes/Views/MainViewController.swift
+++ Recipes/Recipes/Views/MainViewController.swift
@@ -9,25 +9,63 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    let networkClient = RecipesNetworkClient()
+    var allRecipes: [Recipe] = [] {
+        didSet{
+            filterRecipes()
+        }
+    }
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet{
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    var filteredRecipes: [Recipe] = [] {
+        didSet{
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
     @IBOutlet weak var textField: UITextField!
     @IBAction func textFieldAction(_ sender: Any) {
+        DispatchQueue.global().async {
+        self.resignFirstResponder()
+            self.filterRecipes()
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        networkClient.fetchRecipes { (recipes, Error) in
+            if (Error != nil){ NSLog(Error as! String)
+                return
+            }
+            else {
+                self.allRecipes = recipes!
+            }
+        }
+        
         // Do any additional setup after loading the view.
     }
     
+    func filterRecipes() {
+        DispatchQueue.main.async{
+            guard let text = self.textField.text, self.textField.text != "" else {
+                self.filteredRecipes = self.allRecipes
+            return
+        }
+            self.filteredRecipes = self.allRecipes.filter() { $0.name.contains(text) || $0.instructions.contains(text) }
+        }
+    }
 
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "tableSegue"{
+            recipesTableViewController = segue.destination as? RecipesTableViewController
+            
+        }
     }
-    */
 
 }
