706c08b9df81c276946188207df250b303d3bc59
Moses Robinson <mosesjrobinson@icloud.com>
2019-01-21 19:52:05 -0800

    setting up main view controller

--- Recipes/Recipes/View Controllers/RecipesTableViewController.swift
+++ Recipes/Recipes/View Controllers/RecipesTableViewController.swift
@@ -9,33 +9,47 @@
 import UIKit
 
 class RecipesTableViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return recipes.count
     }
-
+    
+    let reuseIdentifier = "RecipeCell"
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        let recipe = recipes[indexPath.row]
+        
+        cell.textLabel?.text = recipe.name
+        
         return cell
     }
-
+    
     // MARK: - Navigation
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowDetail" {
-            let recipeDetailVC = segue.destination as! RecipeDetailViewController
+            guard let recipeDetailVC = segue.destination as? RecipeDetailViewController,
+            let indexPath = tableView.indexPathForSelectedRow else {return}
+            
+            recipeDetailVC.recipe = recipes[indexPath.row]
         }
     }
     
     //MARK: - Properties
+    
+    var recipes: [Recipe] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
 }
