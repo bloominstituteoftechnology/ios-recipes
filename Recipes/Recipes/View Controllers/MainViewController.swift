a710c2b952064a0dbb02ea0a299a4c2507de205d
Vijay Das <vijaytdas@gmail.com>
2019-01-28 17:57:24 -0600

    Add Storyboard layout, custom classes, outlets and actions

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Vijay Das on 1/28/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var searchField: UITextField!
+    
+    
+    @IBAction func searchCompleted(_ sender: UITextField) {
+        
+        
+        
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Recipes/Recipes/View Controllers/RecipesDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RecipesDetailViewController.swift
+//  Recipes
+//
+//  Created by Vijay Das on 1/28/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipesDetailViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var recipesTextView: UITextView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
