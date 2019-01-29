3d1a0a13f4b47a87cdbd97474d19bc3d4fc7d829
Vijay Das <vijaytdas@gmail.com>
2019-01-28 21:34:02 -0600

    add prepare for segue in MainVC

--- Recipes/Recipes/View Controllers/MainViewController.swift
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -17,6 +17,8 @@ class MainViewController: UIViewController {
     
     var allRecipes: [Recipe] = []
     
+    var recipesTableViewController: RecipesTableViewController?
+    
     
     @IBAction func searchCompleted(_ sender: UITextField) {
         
@@ -43,14 +45,20 @@ class MainViewController: UIViewController {
     }
     
 
-    /*
+ 
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        
+        if segue.identifier == "tableViewSegue" {
+            
+            let recipiesTVC = segue.destination as! RecipesTableViewController
+            
+            recipesTableViewController = recipiesTVC
+        }
+        
 
+
+
+    }
+ 
 }
