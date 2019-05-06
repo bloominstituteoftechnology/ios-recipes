928de7607621ba5b40b1968143c80300e17e1dce
Michael Redig <mredig@gmail.com>
2019-05-06 15:35:09 -0500

    most of the way through first step

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Michael Redig on 5/6/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+	@IBOutlet var recipeTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+	}
+}
---
+++ Recipes/Recipes/View Controllers/RecipesTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RecipesTableViewController.swift
+//  Recipes
+//
+//  Created by Michael Redig on 5/6/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+	}
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
