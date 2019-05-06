9d935fa04de5b27bcf48de7491b80daa8dc26e33
Diante Lewis-Jolley <diantelewis-jolley@Diantes-MBP.frontier.com>
2019-05-06 18:35:27 -0400

    Finished product

--- Recipe/Recipe/ViewControllers/MainViewController.swift
+++ Recipe/Recipe/ViewControllers/MainViewController.swift
@@ -9,27 +9,60 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    let networkClient = RecipesNetworkClient()
+    var allRecipes: [Recipe] = []
+    var recipesTableViewController: RecipeTableViewController?
+    var filteredRecipes: [Recipe] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        networkClient.fetchRecipes { (allRecipe, error) in
+            if let error = error {
+                NSLog("Error loading recipes: \(error)")
+                return
+            }
+            DispatchQueue.main.async {
+                self.allRecipes = allRecipe ?? []
+            }
+        }
     }
+
+    func filterRecipe() {
+        if let textField = recipeTextField.text, !textField.isEmpty {
+            filteredRecipes = allRecipes.filter({ (recipe) -> Bool in
+                return recipe.name.lowercased().contains(textField)
+            })
+
+        } else {
+            filteredRecipes = allRecipes
+        }
+
+    }
+
+
 
     @IBAction func textFieldtapped(_ sender: Any) {
+        resignFirstResponder()
+        filterRecipe()
     }
     
     
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "ContainerSegue" {
+            recipesTableViewController = (segue.destination as! RecipeTableViewController)
+        }
+
+
     }
-    */
+
 
 
     @IBOutlet weak var recipeTextField: UITextField!
