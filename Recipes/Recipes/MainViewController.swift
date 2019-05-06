b7bc1e3d1ad30335d9d059596b4f634df78414ae
victor <ruizdvictor@gmail.com>
2019-05-06 15:30:27 -0400

    added view controller outlets and actions

---
+++ Recipes/Recipes/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Victor  on 5/6/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    @IBAction func textFieldDidEnd(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
+    
+    
+}
---
+++ Recipes/Recipes/RecipesTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RecipesTableViewController.swift
+//  Recipes
+//
+//  Created by Victor  on 5/6/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import Foundation
