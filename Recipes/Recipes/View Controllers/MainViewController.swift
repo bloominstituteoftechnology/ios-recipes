ef7913bc182cce49ca09f059f4cacb311a9793e5
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-06 12:52:56 -0700

    edits

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Hector Steven on 5/6/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+    }
+	
+	@IBOutlet var editTextView: UITextField!
+	
+	@IBAction func editingDidEndOnExit(_ sender: Any) {
+		
+		
+	}
+}
---
+++ Recipes/Recipes/View Controllers/RecipesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RecipesTableViewController.swift
+//  Recipes
+//
+//  Created by Hector Steven on 5/6/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+	
+	}
+
+
+}
