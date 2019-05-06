c4fbc40cc135d0c356e7dd9eb9173707a5bfb991
Jeffrey Carpenter <spaz926@me.com>
2019-05-06 14:32:59 -0500

    Set up initial UI and connected UI elements to code

---
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Jeffrey Carpenter on 5/6/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    @IBOutlet weak var recipeDirectionsTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
