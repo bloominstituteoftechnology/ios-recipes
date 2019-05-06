cc86280aebcc28e29f2d3acae553897e1c35d1b2
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-06 15:12:12 -0700

    save and load to presistStorre

--- Recipes/Recipes/View Controllers/MainViewController.swift
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -55,6 +55,48 @@ class MainViewController: UIViewController {
 	let networkClient = RecipesNetworkClient()
 	var recipes: [Recipe] = []
 	var filteredRecipes: [Recipe] = []
+	
+	private var readingListURL: URL? {
+		let fileManager = FileManager.default
+		guard let documents = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+		let fileName = "Recipes.plist"
+		let document = documents.appendingPathComponent(fileName)
+		return document
+	}
+}
+
+extension MainViewController {
+	func saveToPersistentStore() {
+		guard let url = readingListURL else { return }
+		
+		do {
+			let encoder = PropertyListEncoder()
+			let data = try encoder.encode(recipes)
+			try data.write(to: url)
+		} catch {
+			NSLog("Error saving book data: \(error)")
+		}
+	}
+	
+	func loadFromPersistentStore() {
+		let fileManager = FileManager.default
+		
+		guard let url = readingListURL,
+			fileManager.fileExists(atPath: url.path) else {
+				print("error: loadFromPersistentStore()")
+				return
+		}
+		
+		do {
+			let data = try Data(contentsOf: url)
+			let decoder = PropertyListDecoder()
+			let decodedrecipies = try decoder.decode([Recipe].self, from: data)
+			recipes = decodedrecipies
+		}catch {
+			NSLog("Error loading book data: \(error)")
+		}
+	}
+	
 }
 
 
