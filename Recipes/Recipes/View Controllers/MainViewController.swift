74d73c0a25222d44c4dc2a0ba2d1e37945752c36
Sameera Leola <pixelandbyte@gmail.com>
2019-05-06 14:40:54 -0500

    Added views to storyboard and created custom classes for views

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Sameera Roussi on 5/6/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UISearchBarDelegate {
+    
+    
+    @IBOutlet weak var receipeSearchBar: UISearchBar!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad() 
+        
+        receipeSearchBar.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Recipes/Recipes/View Controllers/RecipesTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  RecipesTableViewController.swift
+//  Recipes
+//
+//  Created by Sameera Roussi on 5/6/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeTitleCell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+
+}
