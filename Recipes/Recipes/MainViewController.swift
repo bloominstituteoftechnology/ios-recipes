b522c182d9f24e7db491ae3202f35f08d45ea7d6
victor <ruizdvictor@gmail.com>
2019-05-06 17:00:31 -0400

    added method to fetch from api and display on table view

--- Recipes/Recipes/MainViewController.swift
+++ Recipes/Recipes/MainViewController.swift
@@ -11,21 +11,58 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    let networkClient = RecipesNetworkClient()
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
+    }
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    var recipesTableViewController: RecipesTableViewController?
     @IBOutlet weak var textField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetch()
     }
     
+    func filterRecipes() {
+        DispatchQueue.main.async {
+            guard let searchText = self.textField.text, searchText != "" else {
+                self.filteredRecipes = self.allRecipes
+                return
+            }
+            
+            self.filteredRecipes = self.allRecipes.filter { $0.name.lowercased().contains(searchText) || $0.instructions.lowercased().contains(searchText) }
+        }
+    }
+    
+    
+    func fetch() {
+        networkClient.fetchRecipes { (recipes, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            self.allRecipes = recipes ?? []
+        }
+    }
     
     
     @IBAction func textFieldDidEnd(_ sender: Any) {
-        
-        
+        resignFirstResponder()
+        filterRecipes()
     }
     
-    
-    
-    
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EmbedTableView" {
+            guard let destinationVC = segue.destination as? RecipesTableViewController else {return}
+            
+            recipesTableViewController = destinationVC
+        }
+    }
 }
