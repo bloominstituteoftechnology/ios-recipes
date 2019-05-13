dc35aa921c51f6ed01d25d844a8ada71fd71613f
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-13 09:30:00 -0600

    Part 1 completed
    Set up the UI elements of the Main.storyboard with a VC, a Table VC embedded in it, and a Detail VC to display information listed in the cell of the Table VC

---
+++ Recipes/Recipes/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Mitchell Budge on 5/13/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBAction func searchTextFieldEdited(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Recipes/Recipes/RecipeDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Mitchell Budge on 5/13/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var recipeDetailLabel: UILabel!
+    @IBOutlet weak var recipeDetailTextView: UITextView!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
