6b811b0c66c91bb75ca137fa753719157040d85e
Taylor Lyles <hawks93187@gmail.com>
2019-05-13 13:19:43 -0700

    part one

---
+++ Recipes/Recipes/Model/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Taylor Lyles on 5/13/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+	@IBOutlet weak var textOutlet: UIView!
+	
+	@IBAction func textAction(_ sender: Any) {
+	}
+}
