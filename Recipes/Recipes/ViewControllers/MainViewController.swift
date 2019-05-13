4c9394d49b623f82dd4be4c887538b3594fc82c1
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-05-13 15:45:30 -0400

    Setup the StoryBoard and created the view controller files for each scene.

---
+++ Recipes/Recipes/ViewControllers/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Thomas Cacciatore on 5/13/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func mainTextFieldAction(_ sender: Any) {
+    }
+    
+    
+    
+    
+    @IBOutlet weak var mainTextField: UITextField!
+    
+}
---
+++ Recipes/Recipes/ViewControllers/RecipeDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Thomas Cacciatore on 5/13/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var detailTextView: UITextView!
+    
+
+}
