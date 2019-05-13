93e159d82480249ea2dc3683e63be712540b5c57
Hayden Hastings <haydenhastings32@gmail.com>
2019-05-13 16:47:42 -0600

    Added working code in MainViewController file for fetching the information, and filtering everything.

--- Recipes/Recipes/View Controller/MainViewController.swift
+++ Recipes/Recipes/View Controller/MainViewController.swift
@@ -13,22 +13,56 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        networkClient.fetchRecipes { (recipes, error) in
+            if let error = error {
+                NSLog("Error fetching recipes: \(error)")
+                return
+            }
+            DispatchQueue.main.async {
+                self.allRecipies = recipes ?? []
+            }
+        }
     }
     
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ToRecipies" {
+            recipiesTableViewController = segue.destination as? RecipiesTableViewController
+        }
     }
-    */
+ 
     @IBAction func recipieSearch(_ sender: Any) {
-        
+        resignFirstResponder()
+        filterRecipies()
     }
     
     @IBOutlet weak var searchTextField: UITextField!
+    
+    func filterRecipies() {
+        guard let searchTerm = searchTextField.text?.lowercased(), !searchTerm.isEmpty else {
+            filteredRecipies = allRecipies
+            return }
+        let searchRecipes = allRecipies.filter ( { $0.name.lowercased().contains(searchTerm) || $0.instructions.lowercased().contains(searchTerm) } )
+        filteredRecipies = searchRecipes
+        return
+    }
+    
+    var filteredRecipies: [Recipe] = [] {
+        didSet {
+            recipiesTableViewController?.recipes = filteredRecipies
+        }
+    }
+    var allRecipies: [Recipe] = [] {
+        didSet {
+            filterRecipies()
+        }
+    }
+    var recipiesTableViewController: RecipiesTableViewController? {
+        didSet {
+            recipiesTableViewController?.recipes = filteredRecipies
+        }
+    }
+    let networkClient = RecipesNetworkClient()
 }
