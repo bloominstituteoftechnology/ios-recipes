559d638be53ed54139ceb68368790134199c5e3b
Taylor Lyles <hawks93187@gmail.com>
2019-05-13 13:34:23 -0700

    1.3

---
+++ Recipes/Recipes/Model/RecipeDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Taylor Lyles on 5/13/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+	@IBOutlet weak var recipeLabel: UILabel!
+	@IBOutlet weak var recipeText: UITextView!
+}
