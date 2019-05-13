2b7df28c4cd0eb812f026adcc24c5139ca65815e
John Pitts <jgpitts@gmail.com>
2019-05-13 18:49:26 -0400

    wrote updateViews code in detailViewController to populate the details of the recipe name and instructions fixed weird error related to AppDelegate.swift file

--- Recipes/Recipes/ViewControllers/RecipeDetailViewController.swift
+++ Recipes/Recipes/ViewControllers/RecipeDetailViewController.swift
@@ -13,20 +13,29 @@ class RecipeDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func updateViews() {
+        
+        if isViewLoaded {
+        guard let recipe = recipe else {return}
+            
+            labelTitle.text = recipe.name
+            textViewContent.text = recipe.instructions
+        }
+        
+        
     }
-    */
-    
-    var recipe: Recipe?
 
+    
+    var recipe: Recipe? {
+        didSet {
+            updateViews()
+        }
+    }
+
+    @IBOutlet var labelTitle: UILabel!
+    @IBOutlet var textViewContent: UITextView!
+    
 }
