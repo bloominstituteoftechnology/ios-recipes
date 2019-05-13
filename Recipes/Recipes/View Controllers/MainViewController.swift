2c08102b9c6182dd8aa90049038ebf5680793589
Jeremy Taylor <jtaylor@jctaylor.org>
2019-05-12 20:09:47 -0500

    Finished MVP

--- Recipes/Recipes/View Controllers/MainViewController.swift
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -11,23 +11,63 @@ import UIKit
 class MainViewController: UIViewController {
     @IBOutlet weak var searchTextField: UITextField!
     
+    let networkClient = RecipesNetworkClient()
+    
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.filterRecipes()
+            }
+            
+        }
+    }
+    
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
+    private var recipesTableViewController: RecipesTableViewController? {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        networkClient.fetchRecipes { (recipes, error) in
+            if let error = error {
+                NSLog("Error fetching recipes: \(error)")
+            }
+            
+            guard let recipes = recipes else { return }
+            self.allRecipes = recipes
+        }
     }
     
-    @IBAction func searchFinished(_ sender: Any) {
+    func filterRecipes() {
+        if let text = searchTextField.text, text != "" {
+            let results = allRecipes.filter {$0.name.contains(text) || $0.instructions.contains(text)}
+            filteredRecipes = results
+        } else {
+            filteredRecipes = allRecipes
+        }
     }
     
-    /*
+    @IBAction func searchFinished(_ sender: UITextField) {
+        resignFirstResponder()
+        filterRecipes()
+    }
+    
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "EmbedRecipesTableViewController" {
+           recipesTableViewController = (segue.destination as! RecipesTableViewController)
+        }
     }
-    */
-
 }
--- Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -12,21 +12,22 @@ class RecipeDetailViewController: UIViewController {
     @IBOutlet weak var recipeNameLabel: UILabel!
     @IBOutlet weak var recipeTextView: UITextView!
     
+    var recipe: Recipe? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    private func updateViews() {
+        guard let recipe = recipe, isViewLoaded else { return }
+        
+        recipeNameLabel.text = recipe.name
+        recipeTextView.text = recipe.instructions
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateViews()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
