e8fff522961b13fcad825321512ef347eeb8eeab
John Pitts <jgpitts@gmail.com>
2019-05-14 15:52:07 -0400

    fixed 3 small bugs that prevented code from running.  Now runs SAT! Project Complete. address stretch goals later

--- Recipes/Recipes/ViewControllers/MainViewController.swift
+++ Recipes/Recipes/ViewControllers/MainViewController.swift
@@ -13,26 +13,76 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        networkClient?.fetchRecipes(completion: <#T##([Recipe]?, Error?) -> Void#>)
+        networkClient.fetchRecipes { (recipes, error) in   // this is a concurrent call to network
+            
+            if let error = error {
+                NSLog("Error loading students: \(error)")
+                return
+            } else {
+                
+                DispatchQueue.main.async {
+                guard let recipes = recipes else {return}
+                    self.allRecipes = recipes
+                }
+            }
+        }
     }
     
     @IBAction func searchTextFieldExitted(_ sender: Any) {
         
+        resignFirstResponder()
+        filterRecipes()
+
+    }
+    
+    func filterRecipes() {
+        guard let searchTerm = searchTextField.text?.lowercased() else {return}
+        if searchTerm == "" {
+            filteredRecipes = allRecipes
+        } else {
+            filteredRecipes = allRecipes.filter { $0.name.lowercased().contains(searchTerm) || $0.instructions.lowercased().contains(searchTerm) }
+            
+        }
     }
     
 
-    /*
+
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ContainerToTVC" {
+            
+            guard let recipesTVC = segue.destination as? RecipesTableViewController else { return }
+            
+            recipesTableViewController = recipesTVC
+        }
+        
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+
     
     @IBOutlet var searchTextField: UITextField!
-    let networkClient: RecipesNetworkClient?
-    var allRecipes: [Recipe] = []
+    
+    let networkClient = RecipesNetworkClient()
+    
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
+    }
+    // i was told to make filteredRecipes = [] or i did that on my own?
+    
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    } 
+    
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
     
 }
