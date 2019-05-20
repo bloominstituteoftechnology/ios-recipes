5878e0ece75770ddf14474fc3fc06452033a5964
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-05-20 16:37:23 -0400

    Updated MainViewController.

--- Recipes/Recipes/ViewControllers/MainViewController.swift
+++ Recipes/Recipes/ViewControllers/MainViewController.swift
@@ -13,22 +13,60 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        networkClient.fetchRecipes { (recipes, error) in
+            if let error = error {
+                NSLog("Error loading fetched recipes: \(error)")
+                return
+            }
+            guard let recipes = recipes else { return }
+            self.allRecipes = recipes
+        }
     }
     
+    
+    func filterRecipes() {
+        DispatchQueue.main.async {
+            guard let searchTerm = self.mainTextField.text else { return }
+            if searchTerm == "" || searchTerm.isEmpty == true {
+                self.filteredRecipes = self.allRecipes
+            } else {
+                self.filteredRecipes = self.allRecipes.filter( {$0.name.contains(searchTerm) || $0.instructions.contains(searchTerm) })
+            }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TableViewSegue" {
+            recipesTableViewController = segue.destination as? RecipesTableViewController
+        }
+    }
 
     @IBAction func searchTextField(_ sender: Any) {
+        resignFirstResponder()
+        filterRecipes()
     }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Mark: - Properties
+    
+    let networkClient = RecipesNetworkClient()
+    
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
     }
-    */
-
+    
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
     
     @IBOutlet weak var mainTextField: UITextField!
     
