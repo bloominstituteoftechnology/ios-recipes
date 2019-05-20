833dd038ca3969688053e4c18c349f3ee823436d
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-05-20 15:29:02 -0400

    setup storyboard and set classes.

---
+++ Recipes/Recipes/ViewControllers/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Thomas Cacciatore on 5/20/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func searchTextField(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    @IBOutlet weak var mainTextField: UITextField!
+    
+    
+}
