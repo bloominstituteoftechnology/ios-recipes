4cc51d765a9b6de1faa3dd88dad8a6c63b059814
shillwil <alex.shillingford@icloud.com>
2019-06-17 14:54:32 -0600

    RecipeDetailVC and RecipesTableVC steps in part 2 completed

--- Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -12,22 +12,22 @@ class RecipeDetailViewController: UIViewController {
     
     @IBOutlet weak var recipeLabel: UILabel!
     @IBOutlet weak var textView: UITextView!
+    
+    var recipe: Recipe? {
+        didSet {
+            updateViews()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        recipeLabel.text = recipe?.name
+        textView.text = recipe?.instructions
     }
-    */
-
 }
