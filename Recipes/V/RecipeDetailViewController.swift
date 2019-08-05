6e7a5340abae51f7c11f49790936ef379d90dd6d
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-08-05 15:01:06 -0400

    Initial - Added UI elements - Added UI outlets and functions to the created view controllers

---
+++ Recipes/V/RecipeDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Nathan Hedgeman on 8/5/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+    
+    //Properties
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    @IBOutlet weak var recipeDetailsTextView: UITextView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
