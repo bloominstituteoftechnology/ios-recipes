8bc7c181039b2d290be1ef240f54d7e3071cf0be
Jeffrey Santana <SantanaJny@gmail.com>
2019-08-05 17:05:03 -0400

    Adding search feature and alphabetizing list

--- Recipes/Recipes/Controllers/RecipeTableVC.swift
+++ Recipes/Recipes/Controllers/RecipeTableVC.swift
@@ -13,6 +13,11 @@ class RecipeTableVC: UITableViewController {
 	@IBOutlet weak var searchTextField: UITextField!
 	
 	private var recipes = [Recipe]()
+	private var filteredRecipes = [Recipe]() {
+		didSet {
+			tableView.reloadData()
+		}
+	}
 	
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,10 +25,10 @@ class RecipeTableVC: UITableViewController {
 		RecipesNetworkClient().fetchRecipes { (recipes, error) in
 			if let error = error {
 				NSLog("Fatal error: \(error)")
-			} else if let recipes = recipes {
+			} else if let recipes = recipes?.sorted(by: {$0.name < $1.name}) {
 				self.recipes = recipes
 				DispatchQueue.main.async {
-					self.tableView.reloadData()
+					self.filteredRecipes = recipes
 				}
 			}
 		}
@@ -32,21 +37,36 @@ class RecipeTableVC: UITableViewController {
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 		if let insructionsVC = segue.destination as? InstructionsVC,
 			let indexPath = tableView.indexPathForSelectedRow {
-			insructionsVC.recipe = recipes[indexPath.row]
+			insructionsVC.recipe = filteredRecipes[indexPath.row]
 		}
 	}
-
+	
+	@IBAction func searchUpdated(_ sender: UITextField) {
+		if let search = sender.optionalText?.lowercased() {
+			filteredRecipes = recipes.filter({$0.name.lowercased().contains(search)})
+		} else {
+			filteredRecipes = recipes
+		}
+	}
+	
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return recipes.count
+        return filteredRecipes.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath)
 
-        cell.textLabel?.text = recipes[indexPath.row].name
+        cell.textLabel?.text = filteredRecipes[indexPath.row].name
 
         return cell
+	}
+}
+
+extension UITextField {
+	var optionalText: String? {
+		let trimmedText = self.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+		return (trimmedText ?? "").isEmpty ? nil : trimmedText
 	}
 }
