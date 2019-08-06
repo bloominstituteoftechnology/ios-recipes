e90c710a12cf106409815e48b2ba96678d201b97
bradleyyin <bradleyyin@gmail.com>
2019-08-05 19:08:43 -0700

    basic UI and view controller

---
+++ Recipes/Recipes/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Bradley Yin on 8/5/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func endOnExit(_ sender: Any) {
+        
+    }
+    
+}
---
+++ Recipes/Recipes/RecipeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RecipeViewController.swift
+//  Recipes
+//
+//  Created by Bradley Yin on 8/5/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
