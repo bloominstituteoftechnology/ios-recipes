51256cd15c16d57a6c448b1f25fa5d41c689c521
bradleyyin <bradleyyin@gmail.com>
2019-08-05 19:16:26 -0700

    implement recipe vc

--- Recipes/Recipes/RecipesTableViewController.swift
+++ Recipes/Recipes/RecipesTableViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class RecipesTableViewController: UITableViewController {
+    
+    var recipes :  [Recipe] = []{
+        didSet{
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,14 +32,26 @@ class RecipesTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return recipes.count
     }
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath)
+        let recipe = recipes[indexPath.row]
+        cell.textLabel?.text = recipe.name
+        return cell
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "DetailShowSegue"{
+            guard let detailVC = segue.destination as? RecipeViewController, let indexPath = tableView.indexPathForSelectedRow else { return }
+            
+            detailVC.recipe = recipes[indexPath.row]
+        }
+    }
 
 
 
