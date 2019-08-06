a0ff92762f45c7f0ebd8b3f62d0e8d88dfc2e41f
bradleyyin <bradleyyin@gmail.com>
2019-08-05 19:39:11 -0700

    MVP working as designed

--- Recipes/Recipes/MainViewController.swift
+++ Recipes/Recipes/MainViewController.swift
@@ -11,23 +11,59 @@ import UIKit
 class MainViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     
+    let networkClient = RecipesNetworkClient()
+    
+    var allRecipes: [Recipe] = []{
+        didSet{
+            filterRecipes()
+        }
+    }
+    var filteredRecipes: [Recipe] = []{
+        didSet{
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
+    var recipesTableViewController: RecipesTableViewController?{
+        didSet{
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        networkClient.fetchRecipes { (recipes, error) in
+            if let error = error{
+                NSLog("error networking \(error)")
+                return
+            }else{
+                guard let recipes = recipes else { return }
+                DispatchQueue.main.async {
+                    self.allRecipes = recipes
+                }
+                
+            }
+        }
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "tableViewEmbedSegue"{
+            guard let destination = segue.destination as? RecipesTableViewController else {return}
+            recipesTableViewController = destination
+        }
     }
-    */
     @IBAction func endOnExit(_ sender: Any) {
+        resignFirstResponder()
+        filterRecipes()
+    }
+    func filterRecipes(){
+        guard let searchString = textField.text, !searchString.isEmpty else {
+            filteredRecipes = allRecipes
+            return
+        }
+        filteredRecipes = allRecipes.filter({$0.name.contains(searchString) || $0.instructions.contains(searchString)})
         
     }
     
