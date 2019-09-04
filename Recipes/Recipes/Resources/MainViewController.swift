0bced66bc5299185250f34bafd9db93ec5f81415
Fabiola <saga.fabiola@gmail.com>
2019-09-04 09:15:12 -0500

    Added ViewControllers, TableViewController and connected their classes, and connected outlets and actions for UI elements.

---
+++ Recipes/Recipes/Resources/MainViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Fabiola S on 9/4/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    
+    @IBOutlet weak var textField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: IBActions
+    
+    @IBAction func textFieldPressed(_ sender: UITextField) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Recipes/Recipes/Resources/RecipeDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RecipesDetailViewController.swift
+//  Recipes
+//
+//  Created by Fabiola S on 9/4/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    @IBOutlet weak var recipeTextView: UITextView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
