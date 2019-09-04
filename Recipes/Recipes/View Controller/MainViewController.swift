3496543d1daa91a56a63cc009bfaa7448c3502ac
John Kouris <jdkouris@gmail.com>
2019-09-03 20:46:06 -0700

    Set up interface using storyboards and created the necessary code files

---
+++ Recipes/Recipes/View Controller/MainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by John Kouris on 9/3/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func searchTextFieldDidEnd(_ sender: Any) {
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Recipes/Recipes/View Controller/RecipeDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by John Kouris on 9/3/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+    
+    @IBOutlet weak var recipeTitleLabel: UILabel!
+    @IBOutlet weak var recipeInstructionsTextView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
