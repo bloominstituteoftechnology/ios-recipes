4807d861869c218aadb9f81618145d7fadfd395b
John Kouris <jdkouris@gmail.com>
2019-09-04 18:27:21 -0700

    Completed basic search functionality. App almost working

--- Recipes/Recipes/View Controller/MainViewController.swift
+++ Recipes/Recipes/View Controller/MainViewController.swift
@@ -12,26 +12,64 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var searchTextField: UITextField!
     
+    let networkClient = RecipesNetworkClient()
+    
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
+    }
+    
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = self.filteredRecipes
+        }
+    }
+    
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet {
+            self.recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        networkClient.fetchRecipes { (recipes, error) in
+            if error != nil {
+                NSLog("Error fetching recipes")
+                return
+            } else {
+                guard let recipes = recipes else { return }
+                self.allRecipes = recipes
+            }
+        }
     }
     
     @IBAction func searchTextFieldDidEnd(_ sender: Any) {
+        resignFirstResponder()
+        filterRecipes()
     }
     
-    
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "EmbededTableView" {
+            recipesTableViewController = segue.destination as? RecipesTableViewController
+        }
     }
-    */
+    
+    // MARK: - Private methods
+    
+    func filterRecipes() {
+        DispatchQueue.main.async {
+            guard let searchText = self.searchTextField.text else { return }
+            if searchText.isEmpty || searchText == "" {
+                self.filteredRecipes = self.allRecipes
+            } else {
+                self.filteredRecipes = self.allRecipes.filter { $0.name.contains(searchText) }
+            }
+        }
+    }
 
 }
