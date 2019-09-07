28da149190a4648dbeba8de40160eb1fdd399a91
dillonp23 <dillonp.lambda@gmail.com>
2019-09-06 20:56:03 -0700

    Add code to main view controller. Recipe detail view controller. WIP ability to filter recipes based on search

--- Recipes/View Controllers/MainViewController.swift
+++ Recipes/View Controllers/MainViewController.swift
@@ -12,27 +12,64 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var searchFoodsTextField: UITextField!
     
+    let networkClient = RecipesNetworkClient()
+    
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
+    }
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet {
+            self.recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        networkClient.fetchRecipes { (recipes, error) in
+            if let error = error {
+                NSLog("\(error)")
+                return
+            }
+            guard let recipes = recipes else { return }
+            self.allRecipes = recipes
+        }
         // Do any additional setup after loading the view.
     }
     
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "RecipesTableViewControllerSegue" {
+            guard let recipeTableVC = segue.destination as? RecipesTableViewController else { return }
+            recipesTableViewController = recipeTableVC
+        }
     }
-    */
-    
     
     @IBAction func searchFoods(_ sender: Any) {
+        resignFirstResponder()
+        filterRecipes()
+    }
+    
+    func filterRecipes() {
+        guard let searchTerm = searchFoodsTextField.text else { return }
+        if searchTerm.isEmpty {
+            filteredRecipes = allRecipes
+        } else {
+            filteredRecipes = allRecipes.filter { (searchTerm) -> Bool in
+                return true
+            }
+        }
     }
     
 
--- Recipes/View Controllers/RecipeDetailViewController.swift
+++ Recipes/View Controllers/RecipeDetailViewController.swift
@@ -14,10 +14,16 @@ class RecipeDetailViewController: UIViewController {
     @IBOutlet weak var recipeNameLabel: UILabel!
     @IBOutlet weak var recipeIntructionsTextView: UITextView!
     
+    
+    var recipe: Recipe? {
+        didSet {
+            updateViews()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateViews()
         // Do any additional setup after loading the view.
     }
     
@@ -31,5 +37,12 @@ class RecipeDetailViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    private func updateViews() {
+        guard let recipe = recipe, isViewLoaded else { return }
+        
+        recipeNameLabel.text = recipe.name
+        recipeIntructionsTextView.text = recipe.instructions
+    }
 
 }
