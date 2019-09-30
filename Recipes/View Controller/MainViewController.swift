1b0f6a4cfab60874e82fcec5be5388c8b63ffc45
Andrew Ruiz <nukedoom@gmail.com>
2019-09-30 14:44:23 -0700

    Completed Part I

---
+++ Recipes/View Controller/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Andrew Ruiz on 9/30/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func textFieldAction(_ sender: Any) {
+    }
+    
+}
