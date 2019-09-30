a01696bdc41876d240224162955783fc404581d4
Jesse Ruiz <jesseleeruiz@gmail.com>
2019-09-30 09:41:00 -1000

    Completed Part 1 of project

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Jesse Ruiz on 9/30/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var textField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Actions
+    @IBAction func actionTextField(_ sender: UITextField) {
+    }
+    
+    
+
+}
---
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Jesse Ruiz on 9/30/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var recipeTitle: UILabel!
+    @IBOutlet weak var recipeText: UITextView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
