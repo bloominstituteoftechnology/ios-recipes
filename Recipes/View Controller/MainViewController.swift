f8416073950ac5604750f8879623c7cb3ef136c4
Andrew Ruiz <nukedoom@gmail.com>
2019-09-30 18:03:04 -0700

    Completed filter

--- Recipes/View Controller/MainViewController.swift
+++ Recipes/View Controller/MainViewController.swift
@@ -9,16 +9,64 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
     @IBOutlet weak var searchTextField: UITextField!
+    
+    let networkClient = RecipesNetworkClient()
+    
+    var allRecipes: [Recipe] = []
+    
+    var recipesTableViewController: RecipesTableViewController?
+    
+    var filteredRecipes: [Recipe] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        networkClient.fetchRecipes { (allRecipes, error) in
+            if let error = error {
+                NSLog("Error fetching recipes: \(error)")
+                return
+            } else {
+                guard let allRecipes = allRecipes else { return }
+                DispatchQueue.main.async {
+                    self.allRecipes = allRecipes
+                }
+            }
+        }
+    }
+    
+    // MARK: - Functions
+    
+    func filterRecipes() {
+        
+        // What does "if there is no search term" mean?
+        guard let searchText = searchTextField.text
+            else {
+                filteredRecipes = allRecipes
+                return
+        }
+        
+        // If there is a non-empty search term in the text field, using the filter higher-order function to filter the allRecipes array. It should filter by checking if the recipe's name or instructions contains the search term. Set the value of the filteredRecipes to the result of the filter method.
+        
+        filteredRecipes = allRecipes.filter({ $0.name == searchText || $0.instructions == searchText })
     }
     
     
     @IBAction func textFieldAction(_ sender: Any) {
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "showTable" {
+            guard let tableVC = segue.destination as? RecipesTableViewController else { return }
+            
+            recipesTableViewController = tableVC
+        }
     }
     
 }
