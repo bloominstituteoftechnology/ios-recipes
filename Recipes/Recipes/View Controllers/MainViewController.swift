bf3817efe0c22e8b30632876d40ef617f013f4c8
Dan M <djmthedev@gmail.com>
2019-10-28 16:20:48 -0700

    implemented fuzzy search on recipe title

--- Recipes/Recipes/View Controllers/MainViewController.swift
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -31,10 +31,10 @@ class MainViewController: UIViewController {
             recipesTableViewController?.recipes = filteredRecipes
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         searchBar.delegate = self
         networkClient.fetchRecipes { (recipes, error) in
             if let error = error {
@@ -51,25 +51,54 @@ class MainViewController: UIViewController {
         }
     }
     
-//    @IBAction func editingEnded(_ sender: Any) {
-//        resignFirstResponder()
-//        filterRecipes()
-//    }
+    //    @IBAction func editingEnded(_ sender: Any) {
+    //        resignFirstResponder()
+    //        filterRecipes()
+    //    }
     
     func filterRecipes(on searchTerm: String) {
         
         if searchTerm == "" {
-            print("Hello")
             filteredRecipes = allRecipes
         } else {
-            filteredRecipes = allRecipes.filter({ $0.name.contains(searchTerm) || $0.instructions.contains(searchTerm)
+            filteredRecipes = allRecipes.filter({
+                fuzzySearch(if: searchTerm, isIn: $0.name) ||
+                $0.instructions.contains(searchTerm)
+                
             })
         }
     }
     
+    func fuzzySearch(if item: String, isIn string: String) -> Bool {
+        
+        var testString = ""
+        var tempItem = item.lowercased()
+        var tempString = string.lowercased()
+        
+        if item.isEmpty {
+            return false
+        }
+        
+        while tempString.count >= 1 && tempItem.count >= 1 {
+            var checkLetter = tempItem.remove(at: tempItem.startIndex)
+            
+            for _ in 1...tempString.count {
+                let letter = tempString.remove(at: string.startIndex)
+                if letter == checkLetter {
+                    testString.append(letter)
+                    if testString == item.lowercased() {
+                        return true
+                    }
+                    checkLetter = tempItem.remove(at: tempItem.startIndex)
+                }
+            }
+        }
+        return item == testString
+    }
+    
     
     // MARK: - Navigation
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == PropertyKeys.embedSegue {
             recipesTableViewController = segue.destination as? RecipesTableViewController
