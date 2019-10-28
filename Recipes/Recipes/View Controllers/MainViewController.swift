1c0f651276c58b11cb802005d4a0bebd293e9d47
Dan M <djmthedev@gmail.com>
2019-10-28 07:10:18 -0700

    built out MainViewController

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,76 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by morse on 10/27/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    
+    struct PropertyKeys {
+        static let embedSegue = "TableViewEmbedSegue"
+    }
+    
+    let networkClient = RecipesNetworkClient()
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
+    }
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        networkClient.fetchRecipes { (recipes, error) in
+            if let error = error {
+                NSLog("Error fetching recipe array: \(error)")
+                return
+            }
+            guard let allRecipes = recipes else { return }
+            
+        }
+    }
+    
+    @IBAction func editingEnded(_ sender: Any) {
+        resignFirstResponder()
+        filterRecipes()
+    }
+    
+    func filterRecipes() {
+        guard let searchTerm = textField.text, !searchTerm.isEmpty else {
+            filteredRecipes = allRecipes
+            return
+        }
+        filteredRecipes = allRecipes.filter({ $0.name.contains(searchTerm) || $0.instructions.contains(searchTerm)
+        })
+    }
+    
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == PropertyKeys.embedSegue {
+            guard let recipesTableVC = segue.destination as? RecipesTableViewController else { return }
+            recipesTableVC.recipes = allRecipes
+        }
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
