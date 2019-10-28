d75ae452995432861146d99e75aa2af16b062d0f
Brandi <blubrandi0@gmail.com>
2019-10-28 16:08:45 -0600

    Completed the story board layout and created corresponding View Controller files, with outlets to each from the storyboard.

---
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Brandi on 10/28/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    @IBOutlet weak var RecipeNameLabel: UILabel!
+    @IBOutlet weak var RecipeDetailText: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
