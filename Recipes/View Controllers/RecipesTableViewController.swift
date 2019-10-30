b3a85f2f09d655db1c91ea655ec0d64bc2565959
Joseph Rogers <joerogersss2016@gmail.com>
2019-10-29 23:29:21 -0500

    Recipe Table View Controller
    Variables set
    number of rows in section/cellForRowAt implemented
    Segues set

--- Recipes/View Controllers/RecipesTableViewController.swift
+++ Recipes/View Controllers/RecipesTableViewController.swift
@@ -9,38 +9,39 @@
 import UIKit
 
 class RecipesTableViewController: UITableViewController {
+    
+    //MARK: Properties
+    
+    var recipes: [Recipe] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return recipes.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITVCell", for: indexPath)
 
-        // Configure the cell...
-
+        let recipe = recipes[indexPath.row]
+        cell.textLabel?.text = recipe.name
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -77,14 +78,21 @@ class RecipesTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        switch segue.identifier {
+        case "ShowDetailSegue":
+            if let indexPath = tableView.indexPathForSelectedRow,
+                let showDetailVC = segue.destination as? RecipeDetailViewController {
+                showDetailVC.recipe = recipes[indexPath.row]
+            }
+        default:
+            return
+        }
     }
-    */
+    
 
 }
