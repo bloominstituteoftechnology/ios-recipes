0cfad030b98d7d25b2ce335b616ed290eff5ca11
Chad Rutherford <chad_rutherford@outlook.com>
2019-12-02 14:52:12 -0500

    Finished Storyboard Layout

---
+++ Recipes/Recipes/Controller/View Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Chad Rutherford on 12/2/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var instructionsTextView: UITextView!
+}
