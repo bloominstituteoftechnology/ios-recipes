89b8aa301871a1279a6ba7e9135ec378ad089422
Chad Rutherford <chad_rutherford@outlook.com>
2019-12-02 14:46:23 -0500

    Completed Storyboard Layout pt. 2

---
+++ Recipes/Recipes/Controller/View Controllers/RecipesTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RecipesTableViewController.swift
+//  Recipes
+//
+//  Created by Chad Rutherford on 12/2/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipesTableViewController: UITableViewController {
+    
+}
