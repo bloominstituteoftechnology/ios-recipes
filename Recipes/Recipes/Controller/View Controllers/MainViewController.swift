b81286a2448eb8c5c4d37adad55e2064dde3f1e8
Chad Rutherford <chad_rutherford@outlook.com>
2019-12-02 14:35:22 -0500

    Completed Storyboard Layout pt. 1

---
+++ Recipes/Recipes/Controller/View Controllers/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Chad Rutherford on 12/2/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func textFieldEditingEnded(_ sender: UITextField) {
+    }
+}
