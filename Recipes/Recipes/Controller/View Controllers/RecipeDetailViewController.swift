9b49134073c64e56021e1c50adce9ad56ea145af
Chad Rutherford <chad_rutherford@outlook.com>
2019-12-02 14:57:04 -0500

    Started Implementation of 'RecipeDetailViewController'

--- Recipes/Recipes/Controller/View Controllers/RecipeDetailViewController.swift
+++ Recipes/Recipes/Controller/View Controllers/RecipeDetailViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class RecipeDetailViewController: UIViewController {
     
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - Outlets
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var instructionsTextView: UITextView!
+    
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - Properties
+    var recipe: Recipe? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - View Controller Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViews()
+    }
+    
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - Private
+    private func updateViews() {
+        guard let recipe = recipe, self.isViewLoaded else { return }
+        titleLabel.text = recipe.name
+        instructionsTextView.text = recipe.instructions
+    }
 }
