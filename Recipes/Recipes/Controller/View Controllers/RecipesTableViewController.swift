aacfa85c7c121355c13cbe426989ba2dcf2edabc
Chad Rutherford <chad_rutherford@outlook.com>
2019-12-02 15:05:09 -0500

    Started Implementation of 'RecipesTableViewController'

--- Recipes/Recipes/Controller/View Controllers/RecipesTableViewController.swift
+++ Recipes/Recipes/Controller/View Controllers/RecipesTableViewController.swift
@@ -10,4 +10,49 @@ import UIKit
 
 class RecipesTableViewController: UITableViewController {
     
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - Properties
+    var recipes = [Recipe]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - View Controller Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case Segues.recipeEmbed:
+            break
+        case Segues.showRecipeDetail:
+            guard let recipeDetailVC = segue.destination as? RecipeDetailViewController, let indexPath = tableView.indexPathForSelectedRow else { return }
+            let recipe = recipes[indexPath.row]
+            recipeDetailVC.recipe = recipe
+        default:
+            break
+        }
+    }
+    
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - TableView DataSource Methods
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Cells.recipeCell, for: indexPath)
+        let recipe = recipes[indexPath.row]
+        cell.textLabel?.text = recipe.name
+        return cell
+    }
 }
---
+++ Recipes/Recipes/Resources/Constants/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Recipes
+//
+//  Created by Chad Rutherford on 12/2/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Cells {
+    static let recipeCell = "RecipeCell"
+}
+
+struct Segues {
+    static let recipeEmbed = "RecipeEmbed"
+    static let showRecipeDetail = "ShowRecipeDetail"
+}
