8c8c6a18d21cd00b057c853661a75193fa1eb122
Zack Larsen <zacklarsen1@gmail.com>
2019-12-02 22:02:39 -0700

    almost there

--- Recipes/Recipes/Resources/MainViewController.swift
+++ Recipes/Recipes/Resources/MainViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class MainViewController: UIViewController {
     let networkClient = RecipesNetworkClient()
+    var allRecipes: [Recipe] = []
 
     @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
+        networkClient.fetchRecipes(completion: NSLog()
+        var recipesTableViewController: RecipeDetailViewController?
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -21,14 +24,15 @@ class MainViewController: UIViewController {
     @IBAction func searchField(_ sender: Any) {
     }
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination 
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
