d85a6d02d93f726a4a27e57edb1c35c41b98d2ff
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-01-13 16:00:27 -0800

    part 3 done

--- Recipes/Recipes/ViewController/MainViewController.swift
+++ Recipes/Recipes/ViewController/MainViewController.swift
@@ -12,25 +12,67 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     
-    @IBAction func textFieldAction(_ sender: UITextField) {
+    let networkClient = RecipesNetworkClient()
+    
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
     }
     
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = self.filteredRecipes
+        }
+    }
+    
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet {
+            self.recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
+    @IBAction func textFieldAction(_ sender: UITextField) {
+        resignFirstResponder()
+        filterRecipes()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        networkClient.fetchRecipes { recipes, error in
+            if let error = error {
+                NSLog("Error loading recipes: \(error)")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                if let recipes = recipes {
+                    self.allRecipes = recipes
+                    print(self.allRecipes)
+                }
+            }
+        }
     }
     
+    func filterRecipes() {
+        guard let term = textField.text, !term.isEmpty else {
+            filteredRecipes = allRecipes
+            return
+        }
+         
+    filteredRecipes = allRecipes.filter {
+        $0.name.uppercased().contains(term.uppercased())
+        }
+        
+    }
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "TableViewSegue" {
+            guard let recipesTVC = segue.destination as? RecipesTableViewController else {return}
+            recipesTableViewController = recipesTVC
+            print("segue to TVC")
+        }
     }
-    */
-
 }
--- Recipes/Recipes/ViewController/RecipesTableViewController.swift
+++ Recipes/Recipes/ViewController/RecipesTableViewController.swift
@@ -42,7 +42,7 @@ class RecipesTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -51,7 +51,7 @@ class RecipesTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
 
         // Configure the cell...
         let item = recipes[indexPath.row]
