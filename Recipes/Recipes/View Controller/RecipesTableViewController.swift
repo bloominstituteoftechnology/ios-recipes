5a01d261afe416d3ccf4911264a6961ac6e7e27b
Eoin Lavery <me@eoinlavery.com>
2020-01-15 04:45:06 +0000

    Part 2 Completed: Created RecipesTableViewController.swift file, and linked to UITableView in Main.storyboard file. Created detail UIViewController in storyboard, and created RecipeDetailShowSegue from the UITableViewCell.

---
+++ Recipes/Recipes/View Controller/RecipesTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RecipesTableViewController.swift
+//  Recipes
+//
+//  Created by Eoin Lavery on 15/01/2020.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
