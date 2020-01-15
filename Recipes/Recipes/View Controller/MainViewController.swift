baf54d8ef3b075719c5837be873490c0334adace
Eoin Lavery <me@eoinlavery.com>
2020-01-15 04:41:25 +0000

    Part 1 Completed: Created MainViewController.swift file, and linked to Main.storyboard scene

---
+++ Recipes/Recipes/View Controller/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Eoin Lavery on 15/01/2020.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    //MARK: - IBOutlets
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    //MARK: - Properties
+    
+    //MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //MARK: - IBActions
+    @IBAction func editingDidEnd(_ sender: Any) {
+        
+    }
+    
+}
