5b437d39b893315e77174e3141c808e6a2170803
Eoin Lavery <me@eoinlavery.com>
2020-01-15 04:49:48 +0000

    Part 3 Completed: Added UI elements to RecipeDetailViewController scene in Main.storyboard, and created RecipeDetailViewController.swift file. Linked IBOutlets for the UITextField and UITextView objects.

---
+++ Recipes/Recipes/View Controller/RecipeDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Eoin Lavery on 15/01/2020.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    //MARK: - IBOutlets
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    @IBOutlet weak var recipeTextView: UITextView!
+    
+    //MARK: - Properties
+    
+    //MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
--- Recipes/Recipes/View Controller/RecipesTableViewController.swift
+++ Recipes/Recipes/View Controller/RecipesTableViewController.swift
@@ -14,5 +14,4 @@ class RecipesTableViewController: UITableViewController {
         super.viewDidLoad()
     }
     
-
 }
