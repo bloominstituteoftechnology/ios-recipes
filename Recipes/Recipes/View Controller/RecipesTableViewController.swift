511db9e2524d868612a71bce0e26203271ff2944
Eoin Lavery <me@eoinlavery.com>
2020-01-15 05:04:16 +0000

    Part 5 Completed: Added array of type Recipe to MainViewController with a didSet property observer. Updated numberOfRowsInSection and cellForRowAt methods for RecipeTableViewContoller and configured it's prepareForSegue method to pass selected Recipe object to the RecipeDetailViewController

--- Recipes/Recipes/View Controller/RecipesTableViewController.swift
+++ Recipes/Recipes/View Controller/RecipesTableViewController.swift
@@ -10,8 +10,42 @@ import UIKit
 
 class RecipesTableViewController: UITableViewController {
 
+    //MARK: - Properties
+    var recipes: [Recipe] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    //MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
+    // MARK: - Table View Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipes.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        guard indexPath.row < recipes.count else { return UITableViewCell() }
+        let recipe = recipes[indexPath.row]
+
+        cell.textLabel?.text = recipe.name
+        
+        return cell
+    }
+
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "RecipeDetailShowSegue" {
+            if let destinationVC = segue.destination as? RecipeDetailViewController,
+                let indexPath = tableView.indexPathForSelectedRow {
+                destinationVC.recipe = recipes[indexPath.row]
+            }
+        }
+    }
+
 }
