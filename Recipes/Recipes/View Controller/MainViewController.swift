68886318009dc347e4cbd2b4ab8466cb47e2c3b4
Eoin Lavery <me@eoinlavery.com>
2020-01-15 05:21:20 +0000

    Completed Project MVP goals

--- Recipes/Recipes/View Controller/MainViewController.swift
+++ Recipes/Recipes/View Controller/MainViewController.swift
@@ -13,17 +13,65 @@ class MainViewController: UIViewController {
     //MARK: - IBOutlets
     @IBOutlet weak var textField: UITextField!
     
-    
     //MARK: - Properties
+    let networkClient = RecipesNetworkClient()
+    
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
+    }
+    
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = self.filteredRecipes
+        }
+    }
+    
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet {
+            self.recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
     
     //MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    //MARK: - IBActions
-    @IBAction func editingDidEnd(_ sender: Any) {
-        
+        networkClient.fetchRecipes { (recipes, error) in
+            guard error == nil else {
+                print("Error when attempting to fetch recipes")
+                return
+            }
+            
+            guard let recipes = recipes else {
+                print("Error: Unable to load recipes")
+                return
+            }
+            
+            self.allRecipes = recipes
+        }
     }
     
+    //MARK: - Private Functions
+    private func filterRecipes() {
+        guard let searchText = textField.text,
+            searchText != nil else {
+            filteredRecipes = allRecipes
+            return
+        }
+        
+        filteredRecipes = allRecipes.filter { $0.name.lowercased().contains(searchText) || $0.instructions.lowercased().contains(searchText)}
+    }
+    
+    //MARK: - IBActions
+    @IBAction func editingDidEnd(_ sender: Any) {
+        resignFirstResponder()
+    }
+    
+    //MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TableViewSegue" {
+            recipesTableViewController = segue.destination as? RecipesTableViewController
+        }
+    }
 }
