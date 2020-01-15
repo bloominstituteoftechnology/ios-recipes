16ef454a07bb2331ed66f531423a09ef2a3ec58f
David Wright <davidtwright89@gmail.com>
2020-01-15 01:23:36 -0500

    Completed the storyboard layout (part 1).

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by David Wright on 1/15/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    // MARK: - IBActions
+
+    @IBAction func searchTextFieldDidEndOnExit(_ sender: UITextField) {
+        
+    }
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
