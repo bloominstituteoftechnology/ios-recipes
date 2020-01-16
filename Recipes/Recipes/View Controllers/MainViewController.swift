840956ba4111d79e47e07e454cd4cec3ce6cb83e
David Wright <davidtwright89@gmail.com>
2020-01-15 23:54:12 -0500

    Finished implementing the MainViewController (part 2).

--- Recipes/Recipes/View Controllers/MainViewController.swift
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -10,33 +10,82 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    // MARK: - Outlets
+    // MARK: - Properties
     
+    let networkClient = RecipesNetworkClient()
+
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
+    }
+    
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
+    // MARK: - Outlets
+
     @IBOutlet weak var searchTextField: UITextField!
     
     // MARK: - IBActions
 
     @IBAction func searchTextFieldDidEndOnExit(_ sender: UITextField) {
+        resignFirstResponder()
+        filterRecipes()
+    }
+    
+    // MARK: - Properties
+    
+    func filterRecipes() {
+        guard let searchTerm = searchTextField.text,
+        searchTerm != "" else {
+            filteredRecipes = allRecipes
+            return
+        }
         
+        filteredRecipes = allRecipes.filter {
+            $0.name.contains(searchTerm) ||
+            $0.instructions.contains(searchTerm)
+        }
     }
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        networkClient.fetchRecipes { (allRecipes, error) in
+            guard error == nil else {
+                print("Error loading recipes: \(error!)")
+                return
+            }
+            
+            guard let allRecipes = allRecipes else {
+                print("Error allRecipes was nil!")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.allRecipes = allRecipes
+            }
+        }
     }
     
-
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard segue.identifier == "EmbededTableVCSegue",
+        let recipesTableVC = segue.destination as? RecipesTableViewController else { return }
+        
+        self.recipesTableViewController = recipesTableVC
     }
-    */
 
 }
