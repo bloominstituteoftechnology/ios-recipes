063e94b7a2730e84b23e34fc1e1060ca1d1605d1
Gerardo <gahernandez02@gmail.com>
2020-01-15 20:56:32 -0800

    Added RecipesTableViewController

---
+++ Recipes/Recipes/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Gerardo Hernandez on 1/15/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func textFieldAction(_ sender: UITextField) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
