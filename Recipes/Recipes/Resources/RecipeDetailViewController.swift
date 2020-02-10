d19b732ea0eaf7581bbf6e31973601fc8f96a688
Chris Gonzales <57194564+supergonzo812@users.noreply.github.com>
2020-02-10 12:23:49 -0800

    storyboard #3 completed

---
+++ Recipes/Recipes/Resources/RecipeDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Chris Gonzales on 2/10/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    @IBOutlet weak var RecipeTitleLabel: UILabel!
+    
+    @IBOutlet weak var RecipeInstructionText: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
