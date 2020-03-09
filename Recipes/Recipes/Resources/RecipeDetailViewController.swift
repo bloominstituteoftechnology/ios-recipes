13971c583003b77aa8795cea8c4e616cc9f72c19
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-03-09 14:32:01 -0700

    Added storyboard, working on recipes tableview

---
+++ Recipes/Recipes/Resources/RecipeDetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Bhawnish Kumar on 3/9/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+    
+    var recipe: Recipe? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    @IBOutlet weak var recipeLabel: UILabel!
+    
+    @IBOutlet weak var recipeTextView: UITextView!
+    
+    
+    
+    func updateViews() {
+        
+        recipeLabel.text = recipe?.name
+        recipeTextView.text = recipe?.instructions
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViews()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Recipes/Recipes/ViewControllers/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Bhawnish Kumar on 3/9/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func searchAction(_ sender: Any) {
+    }
+    
+   
+
+}
