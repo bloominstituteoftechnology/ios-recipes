73cea67c1665f2ba755d141275ddd7dcd66cc325
Nichole Davidson <nicholedavid@gmail.com>
2020-03-09 16:39:59 -0600

    Just in case

--- Recipes/Recipes/View Controller/MainViewController.swift
+++ Recipes/Recipes/View Controller/MainViewController.swift
@@ -9,13 +9,21 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     
     @IBOutlet weak var searchTextField: UITextField!
     
     let networkClient = RecipesNetworkClient()
-    var allRecipes: [Recipe] = []
-    var recipesTableViewController: RecipesTableViewController?
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
+    }
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
     var filteredRecipes: [Recipe] = [] {
         didSet {
             recipesTableViewController?.recipes = filteredRecipes
@@ -24,15 +32,15 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         networkClient.fetchRecipes { recipes, error in
             if let error = error {
                 NSLog("Error loading recipes: \(error)")
                 return
             }
-            
             if let recipes = recipes {
                 DispatchQueue.main.async {
-                self.allRecipes = recipes
+                    self.allRecipes = recipes
                 }
             }
         }
@@ -40,38 +48,33 @@ class MainViewController: UIViewController {
     
     func filterRecipes() {
         var updatedRecipes: [Recipe]
-        let searchString = searchTextField.text
         
         guard case searchTextField.text = searchTextField.text else {
-            if searchString != nil {
-            filteredRecipes = allRecipes
+            if searchTextField.text != nil {
+                filteredRecipes = allRecipes
             } else {
-                updatedRecipes = allRecipes.filter { $0.name == "\(searchString ?? "")" }
-                updatedRecipes = allRecipes.filter { $0.instructions == "\(searchString ?? "")" }
-        }
+                updatedRecipes = allRecipes.filter { $0.name == "\(searchTextField.text ?? "")" }
+                updatedRecipes = allRecipes.filter { $0.instructions == "\(searchTextField.text ?? "")" }
+            }
             
             return
         }
         
         updatedRecipes = self.filteredRecipes
-}
+    }
     
-
     @IBAction func searchTapped(_ sender: Any) {
         resignFirstResponder()
         filterRecipes()
     }
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "RecipeSegue" {
             guard let viewRecipeVC = segue.destination as? RecipesTableViewController else { return }
-            
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    
+            viewRecipeVC.recipes = allRecipes
+        }
     }
 }
