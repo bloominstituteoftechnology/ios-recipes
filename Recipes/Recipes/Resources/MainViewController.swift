bc14594a2351eef81ff232553fca0b358d9816ed
Shawn Gee <shawngee@gmail.com>
2020-03-09 15:58:45 -0400

    MVP

---
+++ Recipes/Recipes/Resources/MainViewController.swift
@@ -0,0 +1,73 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Shawn Gee on 3/9/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func textFieldEndedEditing(_ sender: UITextField) {
+        filterRecipes()
+    }
+    
+    
+    //MARK: - Private
+    
+    private var networkClient = RecipesNetworkClient()
+    
+    private var allRecipes: [Recipe] = [] { didSet { filterRecipes() } }
+    private var filteredRecipes: [Recipe] = [] { didSet { recipesTVC?.recipes = filteredRecipes } }
+    
+    private var recipesTVC: RecipesTableViewController? { didSet { recipesTVC?.recipes = filteredRecipes } }
+    
+    private func fetchRecipes() {
+        networkClient.fetchRecipes { recipes, error in
+            if let error = error {
+                NSLog("There was an error fetching recipes: \(error)")
+                return
+            }
+            
+            if let recipes = recipes {
+                DispatchQueue.main.async {
+                    self.allRecipes = recipes
+                }
+            }
+        }
+    }
+    
+    private func filterRecipes() {
+        guard let searchText = textField.text,
+              !searchText.isEmpty else {
+            filteredRecipes = allRecipes
+            return
+        }
+        
+        filteredRecipes = allRecipes.filter {
+            $0.name.localizedStandardContains(searchText) || $0.instructions.localizedStandardContains(searchText)
+        }
+    }
+    
+    //MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchRecipes()
+    }
+    
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TableViewController" {
+            recipesTVC = segue.destination as? RecipesTableViewController
+        }
+    }
+}
