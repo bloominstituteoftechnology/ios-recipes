892aeb5078ed39f7131301c24e400644115911c8
Nichole Davidson <nicholedavid@gmail.com>
2020-03-09 13:59:37 -0600

    Initial scene and file setup

---
+++ Recipes/Recipes/View Controller/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Nichole Davidson on 3/9/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func searchTapped(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
