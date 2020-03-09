6b2dbb765005afb401f7795f39fc6c16dae1fd79
Christopher DeVito <toolofjesus@protonmail.com>
2020-03-09 19:55:50 -0400

    Added 2nd stretch goal: able to delete and update instructions for recipes.

--- Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class RecipeDetailViewController: UIViewController {
     
+    
     // MARK: - Properties
     var recipe: Recipe? {
         didSet {
             updateViews()
         }
     }
+    var recipeController = RecipeController.recipeController
 
     
     // MARK: - IBOutlets
@@ -23,10 +25,26 @@ class RecipeDetailViewController: UIViewController {
     @IBOutlet var recipeTextView: UITextView!
     
     
+    // MARK: - IBActions
+    @IBAction func editButton(_ sender: Any) {
+        recipeTextView.isEditable = true
+    }
+    
+    @IBAction func saveRecipeButton(_ sender: Any) {
+        if recipeTextView.isEditable == true {
+            guard let name = recipe?.name,
+                let instructions = recipeTextView.text
+                else { return }
+            recipeController.updateRecipe(name: name, instructions: instructions)
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
-
+        
         // Do any additional setup after loading the view.
     }
     
--- Recipes/Recipes/View Controllers/RecipesTableViewController.swift
+++ Recipes/Recipes/View Controllers/RecipesTableViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class RecipesTableViewController: UITableViewController {
     
     // MARK: - Properties
+    
+    let recipeController = RecipeController.recipeController
     var recipes: [Recipe] = [] {
         didSet {
             tableView.reloadData()
@@ -26,31 +28,29 @@ class RecipesTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return recipes.count
+        return recipeController.recipes.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath)
 
-        let recipe = recipes[indexPath.row]
+        let recipe = recipeController.recipes[indexPath.row]
         cell.textLabel?.text = recipe.name
         
 
         return cell
     }
 
-
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            let recipe = recipeController.recipes[indexPath.row]
+            recipeController.deleteRecipe(name: recipe.name)
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            recipes.remove(at: indexPath.row)
+        }
     }
-    */
 
 
     // MARK: - Navigation
@@ -58,7 +58,7 @@ class RecipesTableViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "DetailSegue" {
             guard let detailVC = segue.destination as? RecipeDetailViewController, let index = tableView.indexPathForSelectedRow?.row else { return }
-            let recipe = recipes[index]
+            let recipe = recipeController.recipes[index]
             detailVC.recipe = recipe
         }
     }
