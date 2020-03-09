f1b999b6cfbb7022cc0973264967b8154badbad5
Nichole Davidson <nicholedavid@gmail.com>
2020-03-09 14:13:53 -0600

    Added updateViews

--- Recipes/Recipes/View Controller/RecipeDetailViewController.swift
+++ Recipes/Recipes/View Controller/RecipeDetailViewController.swift
@@ -13,10 +13,26 @@ class RecipeDetailViewController: UIViewController {
     @IBOutlet weak var recipeNameLabel: UILabel!
     @IBOutlet weak var recipeDetailTextView: UITextView!
     
+    var recipe: Recipe? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    func updateViews() {
+        if let recipe = recipe {
+            if self.isViewLoaded {
+        recipeNameLabel.text = recipe.name
+        recipeDetailTextView.text = recipe.instructions
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateViews()
 
-        // Do any additional setup after loading the view.
     }
     
 
