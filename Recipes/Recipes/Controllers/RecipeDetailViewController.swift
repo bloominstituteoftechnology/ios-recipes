c7f8b11b77ab6b0fbf4751bbf9d4724e5d7fb0af
Hector L <hectoremil21@hotmail.com>
2020-03-09 16:28:51 -0400

    Set up detail View and its outlets.

---
+++ Recipes/Recipes/Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Karen Rodriguez on 3/9/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    @IBOutlet weak var recipeTitleLabel: UILabel!
+    @IBOutlet weak var recipeInstructionsView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
