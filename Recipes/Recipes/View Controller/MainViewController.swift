3551aabcf6b3c64a4094ebbdeb5a03b203becaff
Matt Martindale <martindalematt18@yahoo.com>
2020-03-10 22:09:47 -0600

    setup storyboard

---
+++ Recipes/Recipes/View Controller/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Matthew Martindale on 3/10/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var searchTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func searchDidEnd(_ sender: Any) {
+        
+    }
+    
+    
+}
