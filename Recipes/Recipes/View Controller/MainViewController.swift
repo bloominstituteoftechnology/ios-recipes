f40c8fd14b7686e070329a18595c50b29ac40436
Claudia Maciel <clc_80@yahoo.com>
2020-03-11 16:59:18 -0700

    Storyboard Layout Complete

---
+++ Recipes/Recipes/View Controller/MainViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Claudia Contreras on 3/11/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet var recipeSearchBar: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - IBAction
+    @IBAction func searching(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Recipes/Recipes/View Controller/RecipeDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Claudia Contreras on 3/11/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet var recipeTitleLabel: UILabel!
+    @IBOutlet var recipeDetailTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
