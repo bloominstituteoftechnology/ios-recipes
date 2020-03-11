5bf9efb821f833f6ea27932e76b5cb020a28ba62
hai666l <0xconr@protonmail.com>
2020-03-11 18:42:05 -0500

    Part 1 - Storyboard Layout - Step 1

---
+++ Recipes/Recipes/Views/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by conner on 3/11/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var searchTF: UITextField!
+    
+    @IBAction func searchAction(_ sender: Any) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
