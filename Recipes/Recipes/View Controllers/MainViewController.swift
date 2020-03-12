ef93d33186e99af6e614919d43a00b0b2eb26107
Chad Parker <chad@chadparker.com>
2020-03-11 18:54:48 -0700

    Set up Storyboard, add ViewController files, outlets, actions

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Chad Parker on 3/11/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var searchTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func searchTextFieldDidEnd(_ sender: Any) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
---
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Chad Parker on 3/11/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var instructionsTextView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
