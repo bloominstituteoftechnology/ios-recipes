a9e02b764f2424522f3e3b323f79c5df5e46b1e2
Matt Martindale <martindalematt18@yahoo.com>
2020-03-11 20:04:55 -0600

    finished app functionality, added case-insensitive search

--- Recipes/Recipes/View Controller/MainViewController.swift
+++ Recipes/Recipes/View Controller/MainViewController.swift
@@ -11,16 +11,57 @@ import UIKit
 class MainViewController: UIViewController {
     
     @IBOutlet weak var searchTextField: UITextField!
+    
+    let networkClient = RecipesNetworkClient()
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
+    }
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet {
+            self.recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = self.filteredRecipes
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        networkClient.fetchRecipes { (recipe, error) in
+            if error != nil {
+                NSLog("There was an error fetching recipes")
+                return
+            }
+            guard let recipe = recipe else { return }
+            self.allRecipes = recipe
+        }
     }
     
     @IBAction func searchDidEnd(_ sender: Any) {
-        
+        resignFirstResponder()
+        filterRecipes()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TableViewSegue" {
+            let destinationVC = segue.destination as! RecipesTableViewController
+            recipesTableViewController = destinationVC
+        }
+    }
+    
+    func filterRecipes() {
+        DispatchQueue.main.async {
+            guard let searchTextField = self.searchTextField.text?.lowercased(),
+                !searchTextField.isEmpty else { return self.filteredRecipes = self.allRecipes }
+            self.filteredRecipes = self.allRecipes.filter { $0.name.lowercased().contains(searchTextField)
+                || $0.instructions.lowercased().contains(searchTextField)
+            }
+        }
+    }
     
 }
