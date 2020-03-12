4c45efc95b92267375c427d615715078110c0733
hai666l <0xconr@protonmail.com>
2020-03-11 19:33:51 -0500

    Part 2 - View Controller Implementation - RecipesTableViewController

--- Recipes/Recipes/Views/RecipesTableViewController.swift
+++ Recipes/Recipes/Views/RecipesTableViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class RecipesTableViewController: UITableViewController {
+    
+    var recipes: [Recipe] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,23 +30,21 @@ class RecipesTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return recipes.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "recipeCell", for: indexPath)
 
         // Configure the cell...
-
+        cell.textLabel?.text = recipes[indexPath.row].name
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -77,14 +81,15 @@ class RecipesTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let recipeDetailVC = segue.destination as? RecipeDetailViewController else { return }
+        if segue.identifier == "recipeDetailSegue" {
+            guard let indexPath = tableView?.indexPathForSelectedRow else { return }
+            recipeDetailVC.recipe = recipes[indexPath.row]
+        }
     }
-    */
 
 }
