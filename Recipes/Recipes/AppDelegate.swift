16761e58df026f760617b2d07c587b2f2ea1c663
Bob Lauer <bob@lambdaschool.com>
2021-11-16 16:15:11 -0600

    BloomTech

--- Recipes/Recipes/AppDelegate.swift
+++ Recipes/Recipes/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  Recipes
 //
 //  Created by Andrew R Madsen on 8/5/18.
-//  Copyright © 2018 Lambda Inc. All rights reserved.
+//  Copyright © 2021 Bloom Institute of Technology Inc. All rights reserved.
 //
 
 import UIKit
